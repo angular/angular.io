e437fdb7b2b5cb471b6f08b918a8813a63efea49
Alex Wolfe <alex@firebase.com>
2015-02-19 09:51:07 -0800

    Angular Material dependancies

---
+++ public/resources/js/site.js
@@ -0,0 +1,8 @@
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
+}]);
