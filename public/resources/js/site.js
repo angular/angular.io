6f2775b5768cb15496764408fd22b1b6858395c5
Alex Wolfe <thewolfe@gmail.com>
2015-02-24 06:38:30 -0800

    Update to core navigation components

--- public/resources/js/site.js
+++ public/resources/js/site.js
@@ -1,7 +1,23 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue', {
+      'default': '700', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('purple', {
+      'default': '200' // use shade 200 for default, and keep all other shades the same
+    });
+});
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
 }]);
+
+
