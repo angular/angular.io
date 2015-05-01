47e25571fd166c7b81e16639eac1427eefc3f3fe
Alex Wolfe <thewolfe@gmail.com>
2015-05-01 06:49:07 -0700

    updated site file

---
+++ public/resources/js/site.js
@@ -0,0 +1,22 @@
+/*
+* Apllication Module
+*
+*/
+
+var angularIO = angular.module('angularIOApp', ['ngMaterial', 'ngTouch'])
+.config(function($mdThemingProvider) {
+
+  // THEMEING FOR ANGULAR MATERIAL
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue', {
+      'default': '700', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('purple', {
+      'default': '200' // use shade 200 for default, and keep all other shades the same
+    });
+});
