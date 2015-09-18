4813e9e17294e4410358fdfeb8b8b0301b5f8581
Peter Bacon Darwin <pete@bacondarwin.com>
2015-09-18 12:11:54 +0100

    feat(typescipt-package): capture the original module name for re-exports
    This change adds the original module name, for items that have been re-exported
    to the current module, as the `originalModule` property on the `exportDoc`.

--- public/api-builder/typescript-package/processors/readTypeScriptModules.spec.js
+++ public/api-builder/typescript-package/processors/readTypeScriptModules.spec.js
@@ -13,6 +13,17 @@ describe('readTypeScriptModules', function() {
     processor.basePath = path.resolve(__dirname, '../mocks/readTypeScriptModules');
   });
 
+  describe('exportDocs', function() {
+    it('should provide the original module if the export is re-exported', function() {
+      processor.sourceFiles = [ 'publicModule.ts' ];
+      var docs = [];
+      processor.$process(docs);
+
+      var exportedDoc = docs[1];
+      expect(exportedDoc.originalModule).toEqual('privateModule');
+    });
+  });
+
 
   describe('ignoreExportsMatching', function() {
     it('should ignore exports that match items in the `ignoreExportsMatching` property', function() {
