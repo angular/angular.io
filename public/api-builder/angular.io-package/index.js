f6b89d620201447f1b3b109adf469cee69b5630e
Jay Traband <jtraband@comcast.net>
2015-09-17 17:51:32 -0700

    refactoring

--- public/api-builder/angular.io-package/index.js
+++ public/api-builder/angular.io-package/index.js
@@ -74,7 +74,7 @@ module.exports = new Package('angular.io', [basePackage])
 
 .config(function(templateEngine, getInjectables) {
   templateEngine.filters = templateEngine.filters.concat(getInjectables([require('./rendering/trimBlankLines')]));
-  templateEngine.filters = templateEngine.filters.concat(getInjectables([require('./rendering/indentNonMixin')]));
+  templateEngine.filters = templateEngine.filters.concat(getInjectables([require('./rendering/indentForMarkdown')]));
 });
 
 
