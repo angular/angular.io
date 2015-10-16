8e81fcacfe1b5c842413275a4861382536f96146
Ward Bell <wardbell@hotmail.com>
2015-10-15 21:51:13 -0700

    (docs) quickstart: remove/rename getting-started artifacts throughout

--- public/docs/_examples/quickstart/e2e-spec.js
+++ public/docs/_examples/quickstart/e2e-spec.js
@@ -1,12 +1,27 @@
-describe('Protractor quick start test', function() {
-	beforeEach(function() {
-		browser.get('quickstart/index.html');
-	});
+/*global browser, element, by */
+describe('QuickStart E2E Tests', function () {
 
-	// #docregion test
-	it('should display Alice', function() {
- 		expect(element(by.id('output')).getText()).toEqual('Hello Alice');
-	});
+	// #docregion shared
+	var expectedMsg = 'My First Angular 2 App';
+
+	// tests shared across languages
+	function sharedTests(basePath) {
+		beforeEach(function () {
+			browser.get(basePath + 'index.html');
+		});
+
+		it('should display: ' + expectedMsg, function () {
+			expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+		});
+	}
 	// #enddocregion
-});
 
+	describe('QuickStart in JavaScript', function () {
+		sharedTests('quickstart/js/');
+	});
+
+	describe('QuickStart in TypeScript', function () {
+		sharedTests('quickstart/ts/');
+	});
+
+});
