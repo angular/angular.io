963f67ea079c1e336ab2f4e4df1391d1ed060b4a
Ward Bell <wardbell@hotmail.com>
2015-10-20 16:10:44 -0700

    (docs) forms - code maintained by _examples Includes template-syntax.jade clarification on attributes v properties

---
+++ public/docs/_examples/forms/ts/src/app/hero-form.component.ts
@@ -0,0 +1,48 @@
+// #docplaster
+// #docregion
+// #docregion first, final
+import {Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+
+import { Hero } from './hero';
+
+@Component({
+  selector: 'hero-form',
+  templateUrl: 'app/hero-form.component.html',
+  // #docregion directives
+  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
+  // #enddocregion
+})
+export class HeroFormComponent {
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  // #docregion submitted
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+  // #enddocregion submitted
+
+  // #enddocregion final
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+  // #enddocregion first
+
+
+  //////// DO NOT SHOW IN DOCS ////////
+
+  // Reveal in html:
+  //   AlterEgo via form.controls = {{showFormControls(hf)}}
+  showFormControls(form){
+    return form.controls.alterEgo &&
+    // #docregion form-controls
+      form.controls.name.value; // Dr. IQ
+    // #enddocregion form-controls
+  }
+  /////////////////////////////
+
+  // #docregion first, final
+}
+// #enddocregion first, final
