e571d2ed926395f3386f523da905d995927d929b
Ward Bell <wardbell@hotmail.com>
2015-10-23 19:05:17 -0700

    (docs) devguide-displaying-data ready for publication, based on _examples

---
+++ public/docs/_examples/displaying-data/ts/src/app/app.final.ts
@@ -0,0 +1,44 @@
+// #docplaster
+// #docregion final
+// #docregion imports
+import {Component, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
+// #enddocregion imports
+import {Hero} from './hero'
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My favorite hero is: {{myHero.name}}</h2>
+  <p>Heroes:</p>
+  <ul>
+    <li *ng-for="#hero of heroes">
+      {{ hero.name }}
+      </li>
+  </ul>
+  // #docregion message
+  <p *ng-if="heroes.length > 3">There are many heroes!</p>
+  // #enddocregion message
+`,
+  // #docregion directives
+  directives: [CORE_DIRECTIVES]
+  // #enddocregion directives
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
+}
+//#enddocregion final
+/*
+// #docregion final
+
+bootstrap(AppComponent);
+//#enddocregion final
+*/
