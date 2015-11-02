33a3dba37ef524f52e8b9de97610df1903135164
Ward Bell <wardbell@hotmail.com>
2015-11-02 00:28:38 -0800

    (docs) devguide pipes: add example code and display it

---
+++ public/docs/_examples/pipes/ts/src/app/power-boost-calculator.ts
@@ -0,0 +1,21 @@
+// #docregion
+import {Component, FORM_DIRECTIVES} from 'angular2/angular2'
+import {ExponentialStrengthPipe} from './exponential-strength-pipe'
+
+@Component({ 
+  selector: 'power-boost-calculator',
+  template: `
+    <h2>Power Boost Calculator</h2>
+    <div>Normal power: <input [(ng-model)]="power"></div>
+    <div>Boost factor: <input [(ng-model)]="factor"></div>
+    <p>
+      Super Hero Power: {{power | exponentialStrength: factor}}
+    </p>
+  `,
+  pipes: [ExponentialStrengthPipe],
+  directives: [FORM_DIRECTIVES]
+})
+export class PowerBoostCalculator { 
+  power = 5;
+  factor = 1;
+}
