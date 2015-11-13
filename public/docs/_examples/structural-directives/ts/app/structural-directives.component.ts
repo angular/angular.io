92f1690b735aaa2ffb348d5f0cfe3608569ee35a
Filipe Silva <filipematossilva@gmail.com>
2015-11-13 14:47:42 +0000

    docs(Structural Directives): new dev guide chapter closes #499

---
+++ public/docs/_examples/structural-directives/ts/app/structural-directives.component.ts
@@ -0,0 +1,21 @@
+// #docplaster
+// #docregion
+import {Component, Input, Output} from 'angular2/core';
+import {UnlessDirective}          from './unless.directive';
+import {HeavyLoaderComponent}     from './heavy-loader.component';
+
+@Component({
+  selector: 'structural-directives',
+  templateUrl: 'app/structural-directives.component.html',
+  styles: ['button { min-width: 100px; }'],
+  directives: [UnlessDirective, HeavyLoaderComponent]
+})
+export class StructuralDirectivesComponent {
+  heroes = ['Mr. Nice', 'Narco', 'Bombasto'];
+  hero = this.heroes[0];
+  condition = true;
+  isVisible = true;
+  logs: string[] = [];
+  status = 'ready';
+}
+//#enddocregion
