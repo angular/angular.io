8b37d0efc089cba4212eda16c34b27f848ebaece
Deborah Kurata <deborahk@insteptech.com>
2015-11-25 15:14:40 -0800

    Completed first draft.

--- public/docs/_examples/attribute-directives/ts/src/app/highlight.directive.ts
+++ public/docs/_examples/attribute-directives/ts/src/app/highlight.directive.ts
@@ -1,5 +1,5 @@
 // #docregion
-import {Directive, ElementRef, Input, Renderer} from 'angular2/angular2';
+import {Directive, ElementRef, Renderer, Input} from 'angular2/angular2';
 
 @Directive({
   selector: '[highlight]',
@@ -10,16 +10,14 @@ import {Directive, ElementRef, Input, Renderer} from 'angular2/angular2';
 })
 
 export class Highlight {
-  @Input('highlight') color: string;
+  @Input() highlight: string;
   private _defaultColor = 'red';
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
-  onMouseEnter() { this._highlight(this.color || this._defaultColor); }
+  onMouseEnter() { this._highlight(this.highlight || this._defaultColor); }
   onMouseLeave() { this._highlight(null); }
 
   private _highlight(color:string) {
-    // this.el.nativeElement.style.backgroundColor=color;
-    // this.el.nativeElement.setAttribute('background-color', color);
     this.renderer.setElementStyle(this.el, 'background-color', color);
   }
 
