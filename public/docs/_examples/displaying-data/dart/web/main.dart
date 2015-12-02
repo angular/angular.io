7421312a7bd33b502f52aaf87e6ad5bd51978ee6
Tom Ingebretsen <tom.ingebretsen@sendoutcards.com>
2015-12-01 23:01:36 -0700

    docs(guide): add _examples/displaying-data/dart
    closes #434

---
+++ public/docs/_examples/displaying-data/dart/web/main.dart
@@ -0,0 +1,14 @@
+// #docregion
+import 'package:angular2/bootstrap.dart';
+//import 'package:displaying_data/app_component_1.dart' as v1;
+//import 'package:displaying_data/app_component_2.dart' as v2;
+//import 'package:displaying_data/app_component_3.dart' as v3;
+import 'package:displaying_data/app_component.dart';
+
+main() {
+// pick one
+//  bootstrap(v1.AppComponent);
+//  bootstrap(v2.AppComponent);
+//  bootstrap(v3.AppComponent);
+  bootstrap(AppComponent);
+}
