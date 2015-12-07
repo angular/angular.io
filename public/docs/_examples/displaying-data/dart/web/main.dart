d0b181d1fc5278127d7804d92088e9c4e327b0c2
Tom Ingebretsen <tom.ingebretsen@sendoutcards.com>
2015-12-07 07:04:53 -0700

    docs(displaying-data): Update to ng alpha48. Remove reference to `ctor` from `main.dart`

--- public/docs/_examples/displaying-data/dart/web/main.dart
+++ public/docs/_examples/displaying-data/dart/web/main.dart
@@ -1,9 +1,8 @@
 // #docregion
 import 'package:angular2/bootstrap.dart';
-import 'package:displaying_data/app_ctor_component.dart';
-import 'package:displaying_data/app_component_1.dart' as v1;
-import 'package:displaying_data/app_component_2.dart' as v2;
-import 'package:displaying_data/app_component_3.dart' as v3;
+//import 'package:displaying_data/app_component_1.dart' as v1;
+//import 'package:displaying_data/app_component_2.dart' as v2;
+//import 'package:displaying_data/app_component_3.dart' as v3;
 import 'package:displaying_data/app_component_final.dart' as v4;
 
 main() {
@@ -11,6 +10,4 @@ main() {
 //  bootstrap(v2.AppComponent);
 //  bootstrap(v3.AppComponent);
   bootstrap(v4.AppComponent);
-  bootstrap(AppCtorComponent);
 }
-// #enddocregion
