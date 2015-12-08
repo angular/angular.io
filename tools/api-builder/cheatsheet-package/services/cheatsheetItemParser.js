3ede7bfea9190600bb5ad96a98ee1acc81c95259
Peter Bacon Darwin <pete@bacondarwin.com>
2015-12-08 21:49:32 +0000

    chore(doc-gen): new syntax for cheatsheet items

--- tools/api-builder/cheatsheet-package/services/cheatsheetItemParser.js
+++ tools/api-builder/cheatsheet-package/services/cheatsheetItemParser.js
@@ -29,57 +29,92 @@
  * }
  * ```
  */
-module.exports = function cheatsheetItemParser() {
-
+module.exports = function cheatsheetItemParser(targetEnvironments) {
   return function(text) {
-    var index = 0;
+    var fields = getFields(text, ['syntax', 'description']);
+
     var item = {
       syntax: '',
       bold: [],
       description: ''
     };
 
-    var STATES = {
-      inSyntax: function() {
-        if (text.charAt(index) !== '`') throw new Error('item syntax must start with a backtick');
-        index += 1;
-        var syntaxStart = index;
-        while(index < text.length && text.charAt(index) !== '`') index++;
-        if (index === text.length) throw new Error('item syntax must end with a backtick');
-        item.syntax = text.substring(syntaxStart, index);
-        state = STATES.pipe;
-        index++;
-      },
-      pipe: function() {
-        if (text.charAt(index) === '|') {
-          index++;
-          while(index < text.length && /\s/.test(text.charAt(index))) index++;
-          state = STATES.bold;
-        } else {
-          state = STATES.description;
+    fields.forEach(function(field) {
+      if (!field.languages || targetEnvironments.someActive(field.languages)) {
+        switch(field.name) {
+          case 'syntax':
+            parseSyntax(field.value.trim());
+            break;
+          case 'description':
+            item.description = field.value.trim();
+            break;
         }
-      },
-      bold: function() {
-        if (text.charAt(index) !== '`') throw new Error('bold matcher must start with a backtick');
-        index += 1;
-        var boldStart = index;
-        while(index < text.length && text.charAt(index) !== '`') index++;
-        if (index === text.length) throw new Error('bold matcher must end with a backtick');
-        item.bold.push(text.substring(boldStart, index));
-        state = STATES.pipe;
-        index++;
-      },
-      description: function() {
-        item.description = text.substring(index);
-        state = null;
       }
-    };
-
-    var state = STATES.inSyntax;
-    while(state) {
-      state();
-    }
+    });
 
     return item;
+
+    function parseSyntax(text) {
+      var index = 0;
+
+      if (text.charAt(index) !== '`') throw new Error('item syntax must start with a backtick');
+
+      var start = index + 1;
+      index = text.indexOf('`', start);
+      if (index === -1) throw new Error('item syntax must end with a backtick');
+      item.syntax = text.substring(start, index);
+      start = index + 1;
+
+      // skip to next pipe
+      while(index < text.length && text.charAt(index) !== '|') index += 1;
+
+      while(text.charAt(start) === '|') {
+
+        start += 1;
+
+        // skip whitespace
+        while(start < text.length && /\s/.test(text.charAt(start))) start++;
+
+        if (text.charAt(start) !== '`') throw new Error('bold matcher must start with a backtick');
+
+        start += 1;
+        index = text.indexOf('`', start);
+        if (index === -1) throw new Error('bold matcher must end with a backtick');
+        item.bold.push(text.substring(start, index));
+        start = index + 1;
+
+      }
+
+      if (start !== text.length) {
+        throw new Error('syntax field must only contain a syntax code block and zero or more bold ' +
+                        'matcher code blocks, delimited by pipes.\n' +
+                        'Instead it was "' + text + '"');
+      }
+    }
   };
+}
+
+
+function getFields(text, fieldNames) {
+  var FIELD_START = /^([^:(]+)\(?([^)]+)?\)?:$/;
+  var lines = text.split('\n');
+  var fields = [];
+  var field, line;
+  while(lines.length) {
+    line = lines.shift();
+    var match = FIELD_START.exec(line);
+    if (match && fieldNames.indexOf(match[1]) !== -1) {
+      // start new field
+      if (field) {fields.push(field);}
+      field = { name: match[1], languages: (match[2] && match[2].split(' ')), value: ''};
+    } else {
+      if (!field) throw new Error('item must start with one of the following field specifiers:\n' +
+                            fieldNames.map(function(field) { return field + ':'; }).join('\n') + '\n' +
+                            'but instead it contained: "' + text + '"');
+      field.value += line + '\n';
+    }
+  }
+  if (field) {fields.push(field);}
+
+  return fields;
 }
