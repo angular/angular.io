fbf98f48c84fc603512330aa8449fa56a7519fe7
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-12-08 20:19:32 -0600

    devguide(display-data) moving TS content over to JS

--- public/docs/_examples/displaying-data/js/src/app/app.1.js
+++ public/docs/_examples/displaying-data/js/src/app/app.1.js
@@ -1,11 +1,9 @@
-// #docplaster
-// #docregion start
+// #docregion
+(function() {
 
 var MyApp = ng.Component({
   selector: 'my-app',
-  // #docregion template
   templateUrl: 'app/app.1.html'
-  // #enddocregion template
 })
 .Class({
   constructor: function() {
@@ -13,10 +11,10 @@ var MyApp = ng.Component({
     this.myHero = 'Windstorm';
   }
 });
-// #enddocregion start
 
-// #docregion bootstrap
 document.addEventListener('DOMContentLoaded', function () {
   ng.bootstrap(MyApp);
 });
-//#enddocregion bootstrap
+
+})();
+// #enddocregion
