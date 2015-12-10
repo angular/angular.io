79fb2df6987e32fe2c6b944b7215978028a82965
Peter Bacon Darwin <pete@bacondarwin.com>
2015-12-10 13:04:00 +0000

    refactor(docs-app): move `bold` directive out of the app controller file

--- public/resources/js/controllers/app-controller.js
+++ public/resources/js/controllers/app-controller.js
@@ -3,27 +3,6 @@
  *
  */
 
-angularIO.directive('bold', function ($timeout) {
-  return {
-    scope: { bold: '=bold' },
-    link:  postLink
-  };
-  function postLink (scope, element) {
-    var bold = typeof scope.bold === 'string'
-        ? [ scope.bold ]
-        : scope.bold;
-    $timeout(function () {
-      var html = element.html();
-      angular.forEach(bold, function (bold) {
-        html = html.replace(new RegExp(bold.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), '<b>$&</b>');
-      });
-      html = html.replace(/\n/g, '<br>');
-      html = html.replace(/ /g, '&nbsp;');
-      element.html(html);
-    });
-  }
-});
-
 angularIO.controller('AppCtrl', ['$mdDialog', '$timeout', '$http', '$sce', function ($mdDialog, $timeout, $http, $sce) {
   var vm = this;
 
