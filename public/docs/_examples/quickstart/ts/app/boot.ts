dbc3982aae1b247b2a0b57ccc6842b8420e0c3bb
Ward Bell <wardbell@hotmail.com>
2015-12-10 18:29:14 -0800

    docs(QuickStart): rewritten for 52 and latest recommendations closes #472

---
+++ public/docs/_examples/quickstart/ts/app/boot.ts
@@ -0,0 +1,7 @@
+// #docregion
+import {bootstrap}    from 'angular2/platform/browser'
+// #docregion app-component
+import {AppComponent} from './app.component'
+// #enddocregion app-component
+
+bootstrap(AppComponent);
