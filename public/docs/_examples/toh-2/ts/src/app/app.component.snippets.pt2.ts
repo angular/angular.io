29b932dd825ccdf20bc64bfdeca62a7b8a78e81d
John Papa <john@johnpapa.net>
2015-12-13 16:52:25 -0500

    toh part 2: fixing snippets.

--- public/docs/_examples/toh-2/ts/src/app/app.component.snippets.pt2.ts
+++ public/docs/_examples/toh-2/ts/src/app/app.component.snippets.pt2.ts
@@ -63,3 +63,37 @@
 // #docregion heroes-ngfor-1
   <li *ngFor="#hero of heroes">
 // #enddocregion heroes-ngfor-1
+
+// #docregion styles-1
+  styles:[`
+    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+    .heroes .badge {
+      font-size: small;
+      color: white;
+      padding: 0.1em 0.7em;
+      background-color: #369;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -1px;
+    }
+    .selected { background-color: #EEE; color: #369; }
+  `],
+// #enddocregion styles-1
+
+// #docregion selected-hero-1
+    public selectedHero: Hero;
+// #enddocregion selected-hero-1
+
+// #docregion on-select-1
+    onSelect(hero: Hero) { this.selectedHero = hero; }
+// #enddocregion on-select-1
+
+// #docregion getSelectedClass-1
+    getSelectedClass(hero: Hero) {
+      return { 'selected': hero === this.selectedHero };
+    }
+// #enddocregion getSelectedClass-1
+
