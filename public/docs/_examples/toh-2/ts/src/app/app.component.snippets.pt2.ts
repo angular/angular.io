8053ca8f4b1efd96940c74d5321be0c97c053b60
John Papa <john@johnpapa.net>
2015-12-13 17:13:58 -0500

    toh part 2: removed whitespace

--- public/docs/_examples/toh-2/ts/src/app/app.component.snippets.pt2.ts
+++ public/docs/_examples/toh-2/ts/src/app/app.component.snippets.pt2.ts
@@ -40,51 +40,54 @@
 // #enddocregion ng-if
 
 // #docregion hero-array-1
-  public heroes: HEROES;
+public heroes: HEROES;
 // #enddocregion hero-array-1
 
 // #docregion heroes-template-1
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li>
-      <!-- each hero goes here -->
-    </li>
-  </ul>
+<h2>My Heroes</h2>
+<ul class="heroes">
+  <li>
+    <!-- each hero goes here -->
+  </li>
+</ul>
 // #enddocregion heroes-template-1
 
 // #docregion heroes-ngfor-1
-  <li *ngFor="#hero of heroes">
+<li *ngFor="#hero of heroes">
 // #enddocregion heroes-ngfor-1
 
 // #docregion styles-1
-  styles:[`
-    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-    .heroes .badge {
-      font-size: small;
-      color: white;
-      padding: 0.1em 0.7em;
-      background-color: #369;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -1px;
-    }
-    .selected { background-color: #EEE; color: #369; }
-  `],
+styles:[`
+  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+  
+  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+  
+  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+  
+  .heroes .badge {
+    font-size: small;
+    color: white;
+    padding: 0.1em 0.7em;
+    background-color: #369;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -1px;
+  }
+  .selected { background-color: #EEE; color: #369; }
+`],
 // #enddocregion styles-1
 
 // #docregion selected-hero-1
-    public selectedHero: Hero;
+public selectedHero: Hero;
 // #enddocregion selected-hero-1
 
 // #docregion on-select-1
-    onSelect(hero: Hero) { this.selectedHero = hero; }
+onSelect(hero: Hero) { this.selectedHero = hero; }
 // #enddocregion on-select-1
 
 // #docregion class-selected-1
-    [class.selected]="hero === selectedHero"
+[class.selected]="hero === selectedHero"
 // #enddocregion class-selected-1
 
 // #docregion class-selected-2
