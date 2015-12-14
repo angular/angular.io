6dc9e5d10bc1bd0a688c26300573255e01a38261
Ward Bell <wardbell@hotmail.com>
2015-12-13 22:29:37 -0800

    docs(Template Syntax): correct minor oversights in a.53 update closes #494

--- public/docs/_examples/template-syntax/ts/app/my-click.directive.ts
+++ public/docs/_examples/template-syntax/ts/app/my-click.directive.ts
@@ -1,10 +1,11 @@
+// #docplaster
 import {Directive,  Output, ElementRef, EventEmitter} from 'angular2/core';
 
 @Directive({selector:'[mClick]'})
 export class MyClickDirective {
-  // #docregion myClick-output-1
+  // #docregion my-click-output-1
   @Output('myClick') clicks = new EventEmitter<string>();
- // #enddocregion myClick-output-1
+ // #enddocregion my-click-output-1
   constructor(el: ElementRef){
     el.nativeElement
       .addEventListener('click', (event:Event) => {
@@ -13,14 +14,14 @@ export class MyClickDirective {
   }
 }
 
-// #docregion myClick-output-2
+// #docregion my-click-output-2
 @Directive({
-// #enddocregion myClick-output-2
+// #enddocregion my-click-output-2
   selector:'[myClick2]',
-// #docregion myClick-output-2
+// #docregion my-click-output-2
   outputs:['clicks:myClick']
 })
-// #enddocregion myClick-output-2
+// #enddocregion my-click-output-2
 export class MyClickDirective2 {
   clicks = new EventEmitter<string>();
   constructor(el: ElementRef){
