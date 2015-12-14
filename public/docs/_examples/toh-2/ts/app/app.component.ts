35a4949f4b57f7be7081ba763980738d8b501df0
John Papa <john@johnpapa.net>
2015-12-13 22:32:37 -0500

    toh part 2: typo on ngModel

--- public/docs/_examples/toh-2/ts/app/app.component.ts
+++ public/docs/_examples/toh-2/ts/app/app.component.ts
@@ -14,7 +14,7 @@ interface Hero {
     <div><label>id: </label>{{hero.id}}</div>
     <div>
       <label>name: </label>
-      <div><input [(ng-model)]="hero.name" placeholder="name"></div>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
     </div>
     <h2>My Heroes</h2>
     <ul class="heroes">
