cb533248f0d2b5c224af946c3be427dc0d8e8e69
John Papa <john@johnpapa.net>
2015-12-15 22:45:44 -0500

    toh: final solutions are now runnable for parts 1 and 2

--- public/docs/_examples/toh-1/ts/app/boot.ts
+++ public/docs/_examples/toh-1/ts/app/boot.ts
@@ -1,4 +1,6 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
+// #docregion pt1
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
+// #enddocregion pt1
--- public/docs/_examples/toh-2/ts-snippets/app/boot.ts
+++
@@ -1,6 +0,0 @@
-// #docregion pt1
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
-
-bootstrap(AppComponent);
-// #enddocregion pt1
