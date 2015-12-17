7e86e9411bfbd4effbcc269f8965939d2cb29a69
John Papa <john@johnpapa.net>
2015-12-16 21:47:02 -0500

    docs(tutorial): add ToH part 4 closes #589

---
+++ public/docs/_examples/toh-4/ts/app/hero.js
@@ -0,0 +1,8 @@
+System.register([], function(exports_1) {
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
+//# sourceMappingURL=hero.js.map
---
+++ public/docs/_examples/toh-4/ts/app/mock-heroes.js
@@ -0,0 +1,22 @@
+System.register([], function(exports_1) {
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ]);
+        }
+    }
+});
+// #enddocregion 
+//# sourceMappingURL=mock-heroes.js.map
