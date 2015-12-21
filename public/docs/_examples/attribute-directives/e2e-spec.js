5a6363d7aefbe3beb2346ccf6a6924e8af1087be
Jay Traband <jtraband@comcast.net>
2015-12-21 11:47:34 -0800

    wip - attribute directives

--- public/docs/_examples/attribute-directives/e2e-spec.js
+++ public/docs/_examples/attribute-directives/e2e-spec.js
@@ -11,5 +11,17 @@ describe('Attribute directives', function () {
     expect(element(by.css('h1')).getText()).toEqual(_title);
   });
 
+  it('should be able to select green highlight', function () {
+    var highlightedEle = element(by.cssContainingText('p', 'Highlight me'));
+    var lightGreen = "rgba(144, 238, 144, 1)";
 
+    expect(highlightedEle.getCssValue('background-color')).not.toEqual(lightGreen);
+    // var greenRb = element(by.cssContainingText('input', 'Green'));
+    var greenRb = element.all(by.css('input')).get(0);
+    greenRb.click().then(function() {
+      browser.actions().mouseMove(highlightedEle).perform();
+      expect(highlightedEle.getCssValue('background-color')).toEqual(lightGreen);
+    });
+
+  });
 });
