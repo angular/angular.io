b8063a814eaba3000aef7eece0db0559e0ad1cef
Kathy Walrath <kathyw@google.com>
2016-01-05 16:32:13 -0800

    docs(guide): update Dart forms example & text
    closes #654

--- public/docs/_examples/forms/dart/lib/hero_form_component.dart
+++ public/docs/_examples/forms/dart/lib/hero_form_component.dart
@@ -15,21 +15,19 @@ const List<String> _powers = const [
 
 @Component(
     selector: 'hero-form',
-    templateUrl: 'hero_form_component.html',
-    // Soon specifying directives here will be unnecessary.
-    directives: const [CORE_DIRECTIVES, FORM_DIRECTIVES])
+    templateUrl: 'hero_form_component.html')
 class HeroFormComponent {
   List<String> get powers => _powers;
 // #docregion submitted
   bool submitted = false;
 // #enddocregion submitted
   Hero model = new Hero(18, 'Dr IQ', _powers[0], 'Chuck Overstreet');
-// #docregion submitted
 // #enddocregion no-todo
   // TODO: Remove this when we're done
   String get diagnostic => 'DIAGNOSTIC: $model';
 // #docregion no-todo
 
+// #docregion submitted
   onSubmit() {
     submitted = true;
   }
