ec392ab7a211781079eff1f02c98cbc1290d82e4
Andres Araujo <a.araujo.azua@gmail.com>
2016-01-12 10:31:29 -0600

    docs(guide): add dart version of hierarchical dependency injection
    closes #683

---
+++ public/docs/_examples/hierarchical-dependency-injection/dart/lib/hero_editor_component.dart
@@ -0,0 +1,47 @@
+// #docregion
+import 'package:angular2/angular2.dart';
+import 'package:hierarchical_di/restore_service.dart';
+import 'package:hierarchical_di/hero.dart';
+
+@Component(
+    selector: 'hero-editor',
+    // #docregion providers
+    providers: const [RestoreService],
+    // #enddocregion providers
+    template: '''
+  <div>
+    <span>Name:</span>
+    <input [(ngModel)]="hero.name"/>
+    <div>
+      <button (click)="onSaved()">save</button>
+      <button (click)="onCanceled()">cancel</button>
+    </div>
+  </div>
+  ''')
+class HeroEditorComponent {
+  @Output() final EventEmitter canceled = new EventEmitter();
+  @Output() final EventEmitter saved = new EventEmitter();
+
+  RestoreService<Hero> _restoreService;
+
+  HeroEditorComponent(this._restoreService);
+
+  @Input()
+  set hero(Hero hero) {
+    _restoreService.setItem(hero);
+  }
+
+  Hero get hero {
+    return _restoreService.getItem();
+  }
+
+  onSaved() {
+    saved.add(_restoreService.getItem());
+  }
+
+  onCanceled() {
+    hero = _restoreService.restoreItem();
+    canceled.add(hero);
+  }
+}
+// #enddocregion
