034a5a69437b1cb4c4d54db2ff9fdd7c6c1aeb50
Ward Bell <wardbell@hotmail.com>
2016-01-18 23:54:27 -0800

    docs(template-syntax): Clarify input/output aliasing syntax closes #722

--- public/docs/_examples/template-syntax/ts/app/my-click.directive.ts
+++ public/docs/_examples/template-syntax/ts/app/my-click.directive.ts
@@ -4,14 +4,17 @@ import {Directive,  Output, ElementRef, EventEmitter} from 'angular2/core';
 @Directive({selector:'[myClick]'})
 export class MyClickDirective {
   // #docregion my-click-output-1
-  @Output('myClick') clicks = new EventEmitter<string>();
+  @Output('myClick') clicks = new EventEmitter<string>(); //  @Output(alias) propertyName = ...
  // #enddocregion my-click-output-1
+ 
   constructor(el: ElementRef){
     el.nativeElement
       .addEventListener('click', (event:Event) => {
-        this.clicks.emit('Click!');
+        this._toggle = !this._toggle;
+        this.clicks.emit(this._toggle ? 'Click!' : '');
       });
   }
+  _toggle = false;
 }
 
 // #docregion my-click-output-2
@@ -19,15 +22,18 @@ export class MyClickDirective {
 // #enddocregion my-click-output-2
   selector:'[myClick2]',
 // #docregion my-click-output-2
-  outputs:['clicks:myClick']
+  outputs:['clicks:myClick']  // propertyName:alias
 })
 // #enddocregion my-click-output-2
 export class MyClickDirective2 {
   clicks = new EventEmitter<string>();
+  
   constructor(el: ElementRef){
     el.nativeElement
       .addEventListener('click', (event:Event) => {
-        this.clicks.emit('Click!');
+        this._toggle = !this._toggle;
+        this.clicks.emit(this._toggle ? 'Click2!' : '');
       });
   }
+  _toggle = false;  
 }
