45115cb8a6c6ce3e4d76e96f424d1222cee57753
Kathy Walrath <kathyw@google.com>
2016-01-27 16:23:57 -0800

    tweaked architecture example

--- public/docs/_examples/architecture/dart/web/main.dart
+++ public/docs/_examples/architecture/dart/web/main.dart
@@ -1,9 +1,9 @@
 // #docregion
 import 'package:angular2/bootstrap.dart';
 import 'package:developer_guide_intro/backend_service.dart';
+import 'package:developer_guide_intro/hero_list_component.dart';
 import 'package:developer_guide_intro/hero_service.dart';
 import 'package:developer_guide_intro/logger_service.dart';
-import 'package:developer_guide_intro/hero_list_component.dart';
 
 main() {
   // #docregion bootstrap
