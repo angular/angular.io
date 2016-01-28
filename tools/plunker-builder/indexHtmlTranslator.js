9f692ffb27ef0043d04c5961c9b24281580e8a40
Ward Bell <wardbell@hotmail.com>
2016-01-28 15:35:53 -0800

    Update from beta.0 to beta.1

--- tools/plunker-builder/indexHtmlTranslator.js
+++ tools/plunker-builder/indexHtmlTranslator.js
@@ -35,48 +35,46 @@ var _rxData = [
     pattern: 'script',
     from: 'node_modules/systemjs/dist/system.src.js',
     to:   ['https://code.angularjs.org/tools/system.js', 'https://code.angularjs.org/tools/typescript.js']
-    //to:   ['https://rawgithub.com/systemjs/systemjs/0.19.6/dist/system.js', 'https://code.angularjs.org/tools/typescript.js']
-    // to: ['https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.18.4/system.js', 'https://code.angularjs.org/tools/typescript.js' ]
   },
   {
     pattern: 'script',
     from: 'node_modules/angular2/bundles/angular2.dev.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/angular2.dev.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/angular2.dev.js'
   },
   {
     pattern: 'script',
     from: 'node_modules/angular2/bundles/angular2-all.umd.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/angular2-all.umd.dev.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/angular2-all.umd.dev.js'
   },  
   {
     pattern: 'script',
     from: 'node_modules/angular2/bundles/angular2-polyfills.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/angular2-polyfills.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/angular2-polyfills.js'
   },
   {
     pattern: 'script',
     from: 'node_modules/rxjs/bundles/Rx.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/Rx.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/Rx.js'
   },  
   {
     pattern: 'script',
     from: 'node_modules/rxjs/bundles/Rx.umd.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/Rx.umd.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/Rx.umd.js'
   },  
   {
     pattern: 'script',
     from: 'node_modules/angular2/bundles/router.dev.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/router.dev.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/router.dev.js'
   },
   {
     pattern: 'script',
     from: 'node_modules/angular2/bundles/http.dev.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/http.dev.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/http.dev.js'
   },
   {
     pattern: 'script',
     from: 'node_modules/angular2/bundles/testing.dev.js',
-    to: 'https://code.angularjs.org/2.0.0-beta.0/testing.dev.js'
+    to: 'https://code.angularjs.org/2.0.0-beta.2/testing.dev.js'
   },
   {
     pattern: 'script',
