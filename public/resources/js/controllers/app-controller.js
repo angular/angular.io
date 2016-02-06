565e4cb61e70b6882585bb055566db0f8931fcfe
Josh Otis <josho@ideablade.com>
2016-02-05 23:27:06 -0800

    chore: new flat left navigation panel (all language tracks)

--- public/resources/js/controllers/app-controller.js
+++ public/resources/js/controllers/app-controller.js
@@ -24,6 +24,33 @@ angularIO.controller('AppCtrl', ['$mdDialog', '$timeout', '$http', '$sce', funct
   vm.toggleVersionMenu = function () {
     vm.showMenu = !vm.showMenu;
   };
+  
+  vm.highlightSelectedLink = function highlightSelectedLink() {
+    var leftNav = document.getElementsByClassName('side-nav-secondary')[0];
+    if (!leftNav) {return;}
+    var links = leftNav.getElementsByTagName('a');
+    var path = location.toString();
+    // remove trailing '/'
+    if (path[path.length-1] === '/') {path = path.slice(0, path.length-1);}
+    // remove trailing '/index.html' (11 chars) for matching purposes.
+    if (path.lastIndexOf('/index.html') === path.length-11) {
+      path = path.slice(0, -11);
+    }
+    for (var i=0; i < links.length; i++){
+      // assumes fresh page each time so no need to clear class
+      if (path === links[i].href) {
+        var parent = links[i].parentElement;
+        parent.classList.add('is-selected');
+
+        if(parent.offsetTop > window.innerHeight && parent.scrollIntoView){
+          parent.scrollIntoView(false);    
+         // parent.scrollTop();
+        }
+        break;
+      }
+    }
+  }
+  vm.highlightSelectedLink();
 
   vm.openFeedback = function() {
     var configuration = {
