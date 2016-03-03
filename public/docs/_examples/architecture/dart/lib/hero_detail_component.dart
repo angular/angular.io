d1f55553a952baed31eae5c7748e03e01b8de524
Kathy Walrath <kathyw@google.com>
2016-03-03 15:50:10 -0800

    docs(dart): update samples and quickstart to beta.8

--- public/docs/_examples/architecture/dart/lib/hero_detail_component.dart
+++ public/docs/_examples/architecture/dart/lib/hero_detail_component.dart
@@ -5,6 +5,5 @@ import 'hero.dart';
 
 @Component(selector: 'hero-detail', templateUrl: 'hero_detail_component.html')
 class HeroDetailComponent {
-  @Input()
-  Type hero = Hero;
+  @Input() Hero hero;
 }
