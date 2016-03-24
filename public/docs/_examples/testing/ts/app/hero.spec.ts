803414ae806b5b4192a8efed1e06d66828079f80
Filipe Silva <filipematossilva@gmail.com>
2016-03-24 19:30:26 +0000

    docs(testing): update testing to use toh-5 closes #1003

--- public/docs/_examples/testing/ts/app/hero.spec.ts
+++ public/docs/_examples/testing/ts/app/hero.spec.ts
@@ -1,17 +1,17 @@
 // #docregion
 // #docplaster
 // #docregion base-hero-spec
-import {Hero} from './hero';
+import { Hero } from './hero';
 
 describe('Hero', () => {
 
-  it('has name given in the constructor', () => {
-    let hero = new Hero(1, 'Super Cat');
+  it('has name', () => {
+    let hero: Hero = {id: 1, name: 'Super Cat'};
     expect(hero.name).toEqual('Super Cat');
   });
 
-  it('has id given in the constructor', () => {
-    let hero = new Hero(1, 'Super Cat');
+  it('has id', () => {
+    let hero: Hero = {id: 1, name: 'Super Cat'};
     expect(hero.id).toEqual(1);
   });
   // #enddocregion base-hero-spec
--- public/docs/_examples/testing/ts/app/heroes.component.ts
+++ public/docs/_examples/testing/ts/app/heroes.component.ts
@@ -1,53 +1,50 @@
-import {Component, OnInit} from 'angular2/core';
-import {HeroDetailComponent} from './hero-detail.component';
-import {HeroService} from './hero.service';
-import {Hero} from './hero';
-import {User} from './user';
+// #docplaster
+// #docregion
+import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
 
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+// #docregion metadata
+// #docregion heroes-component-renaming
 @Component({
   selector: 'my-heroes',
+// #enddocregion heroes-component-renaming
   templateUrl: 'app/heroes.component.html',
-  directives: [HeroDetailComponent],
-  styleUrls: ['app/heroes.component.css']
+  styleUrls:  ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
+// #docregion heroes-component-renaming
 })
+// #enddocregion heroes-component-renaming
+// #enddocregion metadata
+// #docregion class
+// #docregion heroes-component-renaming
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
-  currentHero: Hero;
-  userName: string;
+// #enddocregion heroes-component-renaming
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor(private _heroService: HeroService, private _user: User) {
-    this.userName = this._user.name || 'someone';
+  constructor(
+    private _router: Router,
+    private _heroService: HeroService) { }
+
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
-  getSelectedClass(hero: Hero) {return { selected: hero === this.currentHero }};
-
-  onDelete(hero?: Hero) {
-    hero = hero || this.currentHero;
-    let i = this.heroes.indexOf(hero);
-    if (i > -1) {
-      this.heroes.splice(i, 1);
-    }
-    this.currentHero = this.heroes[i] || this.heroes[i - 1];
+  ngOnInit() {
+    this.getHeroes();
   }
 
-  ngOnInit(){
-    this.heroes = this.onRefresh();
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  gotoDetail() {
+    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
   }
-
-  onRefresh() {
-    //console.log('Refreshing heroes');
-    // clear the decks
-    this.currentHero = undefined;
-    this.heroes = [];
-
-    this._heroService.refresh()
-      .then(heroes => this.heroes = heroes);
-
-    return this.heroes;
-  }
-
-  onSelect(hero: Hero) {
-    this.currentHero = hero;
-    console.log(`Hero selected: ` + JSON.stringify(hero));
-  }
+// #docregion heroes-component-renaming
 }
+// #enddocregion heroes-component-renaming
+// #enddocregion class
+// #enddocregion
