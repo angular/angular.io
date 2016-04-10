6532e71fceebce28441e85405535f7396b0ac7b8
Ward Bell <wardbell@hotmail.com>
2016-04-10 12:19:28 -0700

    docs(cb-set-document-title): new cookbook chapter, Ward's tweaks

--- public/docs/_examples/cb-set-document-title/ts/app/app.component.ts
+++ public/docs/_examples/cb-set-document-title/ts/app/app.component.ts
@@ -1,43 +1,29 @@
+// #docplaster
 // #docregion
 // Import the native Angular services.
-import { Component } from "angular2/core";
-import { Title } from "angular2/platform/browser";
+import { Component } from 'angular2/core';
+import { Title }     from 'angular2/platform/browser';
 
 @Component({
-	selector: 'my-app',
-	template: 
-	`
-		<p>
-			Select a title to set on the current HTML document:
-		</p>
+selector: 'my-app',
+template:
+  `<p>
+    Select a title to set on the current HTML document:
+  </p>
 
-		<ul>
-			<li><a (click)="setTitle( 'Good morning!' )">Good morning</a>.</li>
-			<li><a (click)="setTitle( 'Good afternoon!' )">Good afernoon</a>.</li>
-			<li><a (click)="setTitle( 'Good evening!' )">Good evening</a>.</li>
-		</ul>
-	`
+  <ul>
+    <li><a (click)="setTitle( 'Good morning!' )">Good morning</a>.</li>
+    <li><a (click)="setTitle( 'Good afternoon!' )">Good afternoon</a>.</li>
+    <li><a (click)="setTitle( 'Good evening!' )">Good evening</a>.</li>
+  </ul>
+  `
 })
+// #docregion class
 export class AppComponent {
+  public constructor(private _titleService: Title ) { }
 
-	// I initialize the component.
-	public constructor( title :Title ) {
-
-		this._titleService = title;
-
-	}
-
-
-	// ---
-	// PUBLIC METHODS.
-	// ---
-
-
-	// I apply the given title to the HTML document.
-	public setTitle( newTitle :string ) : void {
-
-		this._titleService.setTitle( newTitle );
-		
-	}
-
+  public setTitle( newTitle: string) {
+    this._titleService.setTitle( newTitle );
+  }
 }
+// #enddocregion class
--- public/docs/_examples/cb-set-document-title/ts/app/main.ts
+++ public/docs/_examples/cb-set-document-title/ts/app/main.ts
@@ -1,36 +1,20 @@
 // #docregion
-// #docregion bootstrap-simple
-// Import the native Angular services.
-import { bootstrap } from "angular2/platform/browser";
-import { Title } from "angular2/platform/browser";
+import { bootstrap } from 'angular2/platform/browser';
+import { AppComponent } from './app.component';
 
-// Import the custom application services.
-import { AppComponent } from "./app.component";
+// While Angular supplies a Title service for setting the HTML document title
+// it doesn't include this service as part of the default Browser platform providers.
+// As such, if we want to inject it into the components within our application,
+// we have to explicitly provide the Angular service in our top component.
+// #docregion bootstrap-title
+import { Title } from 'angular2/platform/browser';
 
-var promise = bootstrap( 
-	AppComponent,
-	[
-		// While Angular supplies a Title service for setting the HTML document 
-		// title, it doesn't include this service as part of the default Browser 
-		// platform providers. As such, if we want to inject it into the components
-		// within our application, we have to explicitly provide the Angular
-		// service when bootstrapping our application.
-		Title
-	]
-);
-// #enddocregion bootstrap-simple
-
-// Log bootstrap completion (whether in success or failure).
-promise.then(
-	function handleBootstrapSuccess() {
-
-		console.info( "Angular finished bootstrapping your application!" );
-
-	},
-	function handleBootstrapFailure( error :any ) {
-
-		console.warn( "Angular was not able to bootstrap your application." );
-		console.error( error );
-
-	}
-);
+bootstrap(AppComponent, [ Title ])
+// #enddocregion bootstrap-title
+  .then(
+    () => window.console.info( 'Angular finished bootstrapping your application!' ),
+    (error) => {
+      console.warn( 'Angular was not able to bootstrap your application.' );
+      console.error( error );
+    }
+  );
