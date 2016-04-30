3aa533f61b857515ad943597903b3a3dd795298c
Patrice Chalin <chalin@users.noreply.github.com>
2016-04-30 07:06:00 -0700

    chore(dart): pipes example rolled back to beta.15
    The pipes example stopped working in beta.16; roll it back to the
    last known working version. 
    
    See earlier commit for code changes necessary in support of beta.16:
    https://github.com/angular/angular.io/commit/0557c728d75e519bc6846b37492
    414dff540c6ee

--- public/docs/_examples/pipes/dart/lib/fetch_json_pipe.dart
+++ public/docs/_examples/pipes/dart/lib/fetch_json_pipe.dart
@@ -8,12 +8,11 @@ import 'package:angular2/angular2.dart';
 // #docregion pipe-metadata
 @Pipe(name: 'fetch', pure: false)
 // #enddocregion pipe-metadata
-@Injectable() // FIXME(chalin): unnecessary?
 class FetchJsonPipe extends PipeTransform {
   dynamic _fetchedValue;
   Future<dynamic> _fetchPromise;
 
-  transform(String url) {
+  transform(dynamic url, [List<dynamic> args]) {
     if (_fetchPromise == null) {
       _fetchPromise = new Future(() async {
         _fetchedValue = JSON.decode(await HttpRequest.getString(url));
