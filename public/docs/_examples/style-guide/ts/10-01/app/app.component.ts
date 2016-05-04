b28bdb6a690365815ae2d1adea9bf7389c0b0428
Foxandxss <foxandxss@gmail.com>
2016-05-04 15:58:43 +0200

    docs(style-guide): update 10-01 for new router

--- public/docs/_examples/style-guide/ts/10-01/app/app.component.ts
+++ public/docs/_examples/style-guide/ts/10-01/app/app.component.ts
@@ -1,6 +1,6 @@
 // #docregion
 import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import { Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 
 import { NavComponent } from './shared/nav/nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -17,8 +17,8 @@ import { HeroService } from './heroes/shared/hero.service';
     HeroService
   ]
 })
-@RouteConfig([
-  { path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true },
-  { path: '/heroes/...', name: 'Heroes', component: HeroesComponent },
+@Routes([
+  { path: '/dashboard', component: DashboardComponent }, // , useAsDefault: true}, // coming soon
+  { path: '/heroes/...', component: HeroesComponent },
 ])
 export class AppComponent {}
