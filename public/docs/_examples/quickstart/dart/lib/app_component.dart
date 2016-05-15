c8f805a22e2f4dd7579ce2afcb3cae835bd5607d
Patrice Chalin <pchalin@gmail.com>
2016-05-14 17:43:01 -0700

    chore(quickstart/dart): updated prose and example code
    Recreated prose as extension of TS prose. Code was updated to conform
    to the organization of the app given on the TS side. I.e., the app
    component was factored out into its own file. E2e tests pass :).

---
+++ public/docs/_examples/quickstart/dart/lib/app_component.dart
@@ -0,0 +1,12 @@
+// #docregion
+// #docregion import
+import 'package:angular2/core.dart';
+// #enddocregion import
+
+// #docregion metadata
+@Component(
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>')
+// #enddocregion metadata
+// #docregion class
+class AppComponent {}
