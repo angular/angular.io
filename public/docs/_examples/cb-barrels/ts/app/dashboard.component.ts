a599c86e07af993e68c755bb977b841648b2ebdc
Jim Cooper <jim@cooperplanet.com>
2016-05-16 00:26:36 -0600

    docs(cb-example): Add barrels cookbook docs

---
+++ public/docs/_examples/cb-barrels/ts/app/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
+import { Hero, HeroService } from './heroes';
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private _router: Router) { }
+
+  ngOnInit() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5));
+  }
+
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', { id: hero.id }];
+    this._router.navigate(link);
+  }
+}
---
+++ public/docs/_examples/cb-barrels/ts/app/heroes/index.ts
@@ -0,0 +1,4 @@
+export * from './hero';
+export * from './hero-detail.component';
+export * from './hero.service';
+export * from './heroes.component';
