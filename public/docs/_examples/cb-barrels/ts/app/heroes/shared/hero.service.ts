9f83cf53210583daf0b450aa728f591ee55401d6
Jim Cooper <jim@cooperplanet.com>
2016-05-16 00:26:36 -0600

    docs(cb-barrels): add barrels cookbook

---
+++ public/docs/_examples/cb-barrels/ts/app/heroes/shared/hero.service.ts
@@ -0,0 +1,17 @@
+// #docregion imports
+import { Injectable } from '@angular/core';
+import { HEROES }     from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+// #enddocregion
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+
+	getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+      heroes => heroes.filter(hero => hero.id === id)[0]
+    );
+  }
+}
