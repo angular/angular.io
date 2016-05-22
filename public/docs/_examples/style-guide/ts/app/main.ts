4371465f4ee609ee0f8b0e1c62294e9b8e55282a
Foxandxss <foxandxss@gmail.com>
2016-05-22 16:40:40 +0200

    docs(style-guide): add e2e tests

--- public/docs/_examples/style-guide/ts/app/main.ts
+++ public/docs/_examples/style-guide/ts/app/main.ts
@@ -1,7 +1,8 @@
 import { bootstrap }        from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { XHRBackend, HTTP_PROVIDERS } from '@angular/http';
-import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
 import 'rxjs/add/operator/map';
 
 import { HeroData } from './hero-data';
@@ -10,6 +11,7 @@ import { AppComponent }     from './app.component';
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
   HTTP_PROVIDERS,
+  { provide: LocationStrategy, useClass: HashLocationStrategy },
   { provide: XHRBackend, useClass: InMemoryBackendService },
   { provide: SEED_DATA,  useClass: HeroData }
   ]);
