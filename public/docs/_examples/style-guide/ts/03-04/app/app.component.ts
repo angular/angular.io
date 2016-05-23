564c99b4131c3aca69e07b7f04a6ad2b9d3e641e
Foxandxss <foxandxss@gmail.com>
2016-05-23 15:33:06 +0200

    docs(style-guide): a service doesn't have a folder

--- public/docs/_examples/style-guide/ts/03-04/app/app.component.ts
+++ public/docs/_examples/style-guide/ts/03-04/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ToastService } from './shared/toast/toast.service';
+import { ToastService } from './shared/toast.service';
 
 @Component({
   selector: 'sg-app',
