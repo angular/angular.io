0e137639430eb807ea8efee82bfa91c9760cd953
Foxandxss <foxandxss@gmail.com>
2016-05-23 15:22:23 +0200

    docs(style-guide): 03-02 pluralize villain closes #1479

--- public/docs/_examples/style-guide/ts/03-02/app/app.component.ts
+++ public/docs/_examples/style-guide/ts/03-02/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { HEROES_URL, VILLAIN_URL } from './shared/data.service';
+import { HEROES_URL, VILLAINS_URL } from './shared/data.service';
 
 @Component({
   selector: 'sg-app',
@@ -11,5 +11,5 @@ import { HEROES_URL, VILLAIN_URL } from './shared/data.service';
 })
 export class AppComponent {
   heroesUrl = HEROES_URL;
-  villainsUrl = VILLAIN_URL;
+  villainsUrl = VILLAINS_URL;
 }
