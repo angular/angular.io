6a3c91a11859d918e0d16cd84382aaffdeaa8f38
Foxandxss <foxandxss@gmail.com>
2016-05-25 16:47:18 +0200

    docs: add real quickstart example

---
+++ public/docs/_examples/real-quickstart/ts/app/app.component.ts
@@ -0,0 +1,14 @@
+// #docregion
+// #docregion import
+import { Component } from '@angular/core';
+// #enddocregion import
+
+// #docregion metadata
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
+})
+// #enddocregion metadata
+// #docregion class
+export class AppComponent { }
+// #enddocregion class
