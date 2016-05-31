4a5f074061cce9ceb511720abcb9d49fed66e7ad
Eric Jimenez <eric.jimenez.dev@gmail.com>
2016-05-31 11:33:46 -0400

    add placeholder text when no description is found

--- public/resources/js/controllers/resources-controller.js
+++ public/resources/js/controllers/resources-controller.js
@@ -4,10 +4,19 @@
  * This controller is responsible for fetching all the data for the resources page,
  * from Firebase.
  */
-angularIO.controller('ResourcesCtrl', ['$firebaseArray', '$firebaseObject', function ($firebaseArray, $firebaseObject) {
+angularIO.controller('ResourcesCtrl', ['$firebaseArray', '$firebaseObject','$location', function ($firebaseArray, $firebaseObject, $location) {
+  var DEFAULT_CATEGORY = 'education';
   var categoryRef = new Firebase("https://angularresources.firebaseio.com/");
   var vm = this;
 
   vm.fbObject = $firebaseObject(categoryRef);
+  vm.selectedCategory = $location.hash() ? $location.hash() : DEFAULT_CATEGORY;
+
+  // onSelect :: String
+  // Side effect, modifies vm.selectedCategory
+  vm.onSelectCategory = function onSelectCategory(category) {
+    $location.hash(category);
+    vm.selectedCategory = category;
+  };
 
 }]);
