67bc690dbe0d19f13a76cd3547814c790ab02642
Foxandxss <foxandxss@gmail.com>
2016-06-05 20:25:16 +0200

    docs(cli-quickstart): add cli 5 min quickstart

--- gulpfile.js
+++ gulpfile.js
@@ -223,11 +223,19 @@ function findAndRunE2eTests(filter, outputFile) {
 // fileName; then shut down the example.  All protractor output is appended
 // to the outputFile.
 function runE2eTsTests(appDir, outputFile) {
-  // start the app
-  var appRunSpawnInfo = spawnExt('npm',['run','http-server:e2e', '--', '-s' ], { cwd: appDir });
-  var tscRunSpawnInfo = spawnExt('npm',['run','tsc'], { cwd: appDir });
+  // spawn tasks to start the app
+  var appBuildSpawnInfo;
+  var appRunSpawnInfo;
 
-  return runProtractor(tscRunSpawnInfo.promise, appDir, appRunSpawnInfo, outputFile);
+  if (fs.existsSync(path.join(appDir, 'angular-cli.json'))) {
+    appBuildSpawnInfo = spawnExt('npm', ['run', 'build:cli'], { cwd: appDir });
+    appRunSpawnInfo = spawnExt('npm', ['run', 'http-server:cli', '--', '-s'], { cwd: appDir });
+  } else {
+    appBuildSpawnInfo = spawnExt('npm',['run','tsc'], { cwd: appDir });
+    appRunSpawnInfo = spawnExt('npm',['run','http-server:e2e', '--', '-s' ], { cwd: appDir });
+  }
+
+  return runProtractor(appBuildSpawnInfo.promise, appDir, appRunSpawnInfo, outputFile);
 }
 
 function runProtractor(prepPromise, appDir, appRunSpawnInfo, outputFile) {
