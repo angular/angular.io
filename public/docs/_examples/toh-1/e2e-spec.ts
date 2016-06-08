1fc3592f9fef657b1692f30feeb429f760c39940
Patrice Chalin <pchalin@gmail.com>
2016-06-08 07:40:49 -0700

    test(toh-1): add e2e for Dart and TS
    Contributes to #1619.

---
+++ public/docs/_examples/toh-1/e2e-spec.ts
@@ -0,0 +1,26 @@
+/// <reference path="../_protractor/e2e.d.ts" />
+describe('Tutorial part 1', () => {
+
+  let expectedH1 = 'Tour of Heroes';
+  let expectedTitle = `Angular 2 ${expectedH1}`;
+  let hero = { id: 1, name: 'Windstorm' };
+  let expectedH2 = `${hero.name} details!`;
+
+  beforeEach(() => {
+    return browser.get('');
+  });
+
+  it(`should have title '${expectedTitle}'`, () => {
+    expect(browser.getTitle()).toEqual(expectedTitle);
+  });
+
+  it(`should have '${expectedH2}'`, () => {
+    var text = element(by.css('h2')).getText()
+    expect(text).toEqual(expectedH2);
+  });
+
+  it(`should have input name '${hero.name}'`, () => {
+    var name = element(by.css('input')).getAttribute('value');
+    expect(name).toEqual(hero.name);
+  });
+});
