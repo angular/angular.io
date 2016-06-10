4dcea96b6d2a75dc332874b583fe5e61e8c105a7
Foxandxss <foxandxss@gmail.com>
2016-06-10 13:50:16 +0200

    chore: better protractor task closes #1641

---
+++ public/docs/_examples/webpack/e2e-spec.ts
@@ -0,0 +1,18 @@
+/// <reference path="../_protractor/e2e.d.ts" />
+describe('QuickStart E2E Tests', function () {
+
+  let expectedMsg = 'Hello from Angular 2 App with Webpack';
+
+  beforeEach(function () {
+    browser.get('');
+  });
+
+  it(`should display: ${expectedMsg}`, function () {
+    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  });
+
+  it('should display an image', function () {
+    expect(element(by.css('img')).isPresent()).toBe(true);
+  });
+
+});
