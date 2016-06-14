f8eabf9fa36d087baa29eb2c564af4a4de9f1c2b
Torgeir Helgevold <torgeirh@hotmail.com>
2016-06-13 23:24:06 -0400

    docs(cookbook) - using angular 2 with electron
    cc
    
    text
    
    update
    
    dev

---
+++ public/docs/_examples/cb-electron/ts/app/hero-detail.component.ts
@@ -0,0 +1,60 @@
+// #docplaster
+// #docregion, variables-imports
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+// #enddocregion variables-imports
+import { RouteParams } from '@angular/router-deprecated';
+
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
+})
+// #docregion variables-imports
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  @Output() close = new EventEmitter();
+  error: any;
+  navigated = false; // true if navigated here
+  // #enddocregion variables-imports
+
+  constructor(
+    private heroService: HeroService,
+    private routeParams: RouteParams) {
+  }
+
+  // #docregion ngOnInit
+  ngOnInit() {
+    if (this.routeParams.get('id') !== null) {
+      let id = +this.routeParams.get('id');
+      this.navigated = true;
+      this.heroService.getHero(id)
+          .then(hero => this.hero = hero);
+    } else {
+      this.navigated = false;
+      this.hero = new Hero();
+    }
+  }
+  // #enddocregion ngOnInit
+  // #docregion save
+  save() {
+    this.heroService
+        .save(this.hero)
+        .then(hero => {
+          this.hero = hero; // saved hero, w/ id if new
+          this.goBack(hero);
+        })
+        .catch(error => this.error = error); // TODO: Display error message
+  }
+  // #enddocregion save
+  // #docregion goback
+  goBack(savedHero: Hero = null) {
+    this.close.emit(savedHero);
+    if (this.navigated) { window.history.back(); }
+  }
+  // #enddocregion goback
+}
+
