fb179d96904def16bdf0f119d3790b0d2dcac195
Martin Probst <martin@probst.io>
2016-06-20 23:34:14 -0700

    docs(security): Add security documentation.
    Substantially rewritten, based on original content by Brian Clarkio and
    Ward Bell and contributions from Naomi Black.

---
+++ public/docs/_examples/security/ts/app/app.component.ts
@@ -0,0 +1,20 @@
+// #docregion
+import { Component } from '@angular/core';
+
+import { BypassSecurityComponent } from './bypass-security.component';
+import { InnerHtmlBindingComponent } from './inner-html-binding.component';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Security</h1>
+  <inner-html-binding></inner-html-binding>
+  <bypass-security></bypass-security>
+  `,
+  directives: [
+    BypassSecurityComponent,
+    InnerHtmlBindingComponent,
+  ],
+})
+export class AppComponent {
+}
---
+++ public/docs/_examples/security/ts/app/main.ts
@@ -0,0 +1,8 @@
+// #docregion
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+// #docregion import
+import { AppComponent } from './app.component';
+// #enddocregion import
+
+bootstrap(AppComponent);
