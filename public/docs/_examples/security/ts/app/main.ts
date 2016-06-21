65ca03544fb359f93dbfbc5518419c2973c4cc98
Martin Probst <martin@probst.io>
2016-06-20 19:40:37 -0700

    progress

--- public/docs/_examples/security/ts/app/main.ts
+++ public/docs/_examples/security/ts/app/main.ts
@@ -1,8 +1,8 @@
 // #docregion
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {bootstrap} from '@angular/platform-browser-dynamic';
 
 // #docregion import
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 // #enddocregion import
 
 bootstrap(AppComponent);
