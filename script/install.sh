4c1b4412271a5e01da5a8c1f07826f449514350b
Filipe Silva <filipematossilva@gmail.com>
2016-06-29 16:55:14 +0100

    chore(travis): add preview builds

--- script/install.sh
+++ script/install.sh
@@ -3,7 +3,7 @@
 set -ex -o pipefail
 
 npm install --no-optional
-(cd public/docs/_examples && npm install)
-(cd public/docs/_examples/_protractor && npm install)
+(cd public/docs/_examples && npm install --no-optional)
+(cd public/docs/_examples/_protractor && npm install --no-optional)
 npm run webdriver:update --prefix public/docs/_examples/_protractor
 gulp add-example-boilerplate
