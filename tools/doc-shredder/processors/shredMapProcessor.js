984ee418f62ba51c5d2f7fbbe141f52bb3b6b615
Patrice Chalin <pchalin@gmail.com>
2016-07-05 16:46:44 -0700

    chore: fix "Invalid example" warnings from shred map builder
    The shred map (xref) builder was issuing warnings. This fix includes
    - Adjustments to the shredder map builder itself so that it
    understands, e.g., app-project relative example paths.
    - `**/guide/glossary.jade` now (Jade) `includes` the shared parent
    `glossary.jade` rather than (Harp) importing (via `partial`). This
    fixes `makeExample` path issues in the glossary.
    - Adjusted some `makeExample` paths that were ok for site build, but
    confused the xref tool.

--- tools/doc-shredder/processors/shredMapProcessor.js
+++ tools/doc-shredder/processors/shredMapProcessor.js
@@ -18,25 +18,37 @@ module.exports = function shredMapProcessor(log, createDocMessage) {
       var fragToJadeMap = {};
 
       docs.forEach(function(doc) {
-        var jadePath = path.join(options.jadeDir, doc.fileInfo.relativePath);
+        var relativePath = doc.fileInfo.relativePath;
+        var jadePath = path.join(options.jadeDir, relativePath);
+        var lang = relativePath.substr(0, relativePath.indexOf('\/'));
+        var appProjDirName = jadeBaseFileNameToExampleName(doc.fileInfo.baseName);
         var fragInfoSet = {};
         doc.fragItems.forEach(function(fragItem) {
           var mixinPath = fragItem.mixinPath;
           var fullExamplePath;
+          // Normalize mixinPath: strip out optional trailing '(...)'
+          var mixinPath = mixinPath.replace(/ \([^\)]*\)/,'');
           if ( mixinPath.indexOf('_api') >= 0) {
             var sourcePath = mixinPath.replace('_api/','');
             fullExamplePath = path.join(options.apiExamplesDir, sourcePath);
           } else {
             fullExamplePath = path.join(options.devguideExamplesDir, mixinPath);
           }
-          var region = fragItem.region ? "-" + fragItem.region : '';
-          var extn = path.extname(mixinPath);
-          var basename = path.basename(mixinPath, extn);
-          var fragDir = path.dirname(mixinPath);
-          var fragPath = path.join(fragDir, basename + region + extn) + '.md';
-          var fullFragPath =  path.join(options.fragmentsDir, fragPath);
-
-          var fragInfo = { fragPath: fullFragPath, examplePath: fullExamplePath, exists: fs.existsSync(fullFragPath) };
+          var fragInfo = makeFragInfo(options.fragmentsDir, fullExamplePath, fragItem, mixinPath);
+          if (!fragInfo.exists) {
+            var savedFragInfo = fragInfo;
+            // Assume that mixinPath is actually app-project-folder relative and
+            // prepend  "lang/appProjDirName":
+            var appProjRelPath = mixinPath;
+            mixinPath = appProjDirName + '/' + lang + '/' + mixinPath;
+            fragInfo = makeFragInfo(options.fragmentsDir, fullExamplePath, fragItem, mixinPath);
+            if (fragInfo.exists) {
+              log.info('Ajusted example path (' + doc.fileInfo.baseName + '): ' + appProjRelPath + ' -> ' + mixinPath);
+            } else {
+                fragInfo = savedFragInfo;
+            }
+          }
+          var fragPath = fragInfo.relFragPath;
           fragInfoSet[fragPath] = fragInfo;
           if (fragInfo.exists) {
             var jadePathsSet = fragToJadeMap[fragPath];
@@ -46,7 +58,7 @@ module.exports = function shredMapProcessor(log, createDocMessage) {
             }
             jadePathsSet[jadePath] = jadePath;
           } else {
-            var relativePath = path.relative(".", fullFragPath);
+            var relativePath = path.relative(".", fragInfo.fragPath);
             log.warn(createDocMessage('Invalid example (unable to locate fragment file: "' + relativePath + '")', doc));
           }
         });
@@ -82,13 +94,24 @@ module.exports = function shredMapProcessor(log, createDocMessage) {
   }
 };
 
-function getExampleName(fragPath) {
-  // pattern to isolate base fileName and extension from fragment name
-  var rx = /(.*)\-(.*)\.(.s)/;
-  var r = rx.exec(fragPath);
-  if (r) {
-    return r[1] + '.' + r[3];
-  } else {
-    return fragPath;
-  }
+// TODO: use the functionality in public/resources/js/util.js once it lands.
+function jadeBaseFileNameToExampleName(name) {
+  // Adjust for known cases where chapter name is not the example name.
+	var matches = name.match(/(toh-)pt(\d+)/);
+	if (matches) name = matches[1] + matches[2];
+	return name;
+}
+
+function makeFragInfo(fragmentsDir, fullExamplePath, fragItem, mixinPath) {
+  var region = fragItem.region ? "-" + fragItem.region : '';
+  var extn = path.extname(mixinPath);
+  var basename = path.basename(mixinPath, extn);
+  var fragDir = path.dirname(mixinPath);
+  var fragPath = path.join(fragDir, basename + region + extn) + '.md';
+  var fullFragPath =  path.join(fragmentsDir, fragPath);
+  return {
+    fragPath: fullFragPath,
+    relFragPath: fragPath,
+    examplePath: fullExamplePath, 
+    exists: fs.existsSync(fullFragPath) };
 }
