da58ff45c7896d2f883c751bbd1d520009c6a3c5
Foxandxss <foxandxss@gmail.com>
2016-07-05 15:06:13 +0200

    docs(style-guide): update style-guide samples to use v3 router closes #1828

--- public/docs/_examples/style-guide/ts/app/main.ts
+++ public/docs/_examples/style-guide/ts/app/main.ts
@@ -1,15 +1,15 @@
 import { bootstrap }        from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { XHRBackend, HTTP_PROVIDERS } from '@angular/http';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
 import 'rxjs/add/operator/map';
 
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 import { HeroData } from './hero-data';
 import { AppComponent }     from './app.component';
 
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS,
+  APP_ROUTER_PROVIDERS,
   HTTP_PROVIDERS,
   { provide: LocationStrategy, useClass: HashLocationStrategy },
   { provide: XHRBackend, useClass: InMemoryBackendService },
