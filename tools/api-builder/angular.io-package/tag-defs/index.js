86f82124d458285c4053482b6e9401eddcb859e0
Patrice Chalin <pchalin@gmail.com>
2016-07-05 16:58:31 -0700

    chore: dgeni tag defintions for ts2dart tags closes #1833 New `dgeni` tag definitions for `ts2dart` tags which are currently causing spurious warnings when generating API docs.

--- tools/api-builder/angular.io-package/tag-defs/index.js
+++ tools/api-builder/angular.io-package/tag-defs/index.js
@@ -1,9 +1,11 @@
 module.exports = [
+  require('./Annotation'),
   require('./deprecated'),
   require('./howToUse'),
   require('./whatItDoes'),
   require('./internal'),
   require('./stable'),
+  require('./ts2dart_const'),
   require('./experimental'),
   require('./docsNotRequired'),
   require('./security'),
