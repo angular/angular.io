d1dd2b82f4c0e572b3d4b9c95eeeb5ef51078f2b
Ward Bell <wardbell@hotmail.com>
2016-07-06 01:11:00 -0700

    docs(router): changes suggested by Deb Kurata in #1808

--- public/docs/_examples/router/ts/app/auth.guard.ts
+++ public/docs/_examples/router/ts/app/auth.guard.ts
@@ -1,20 +1,13 @@
 // #docregion
-import { Injectable }             from '@angular/core';
-import { CanActivate,
-         Router,
-         ActivatedRouteSnapshot,
-         RouterStateSnapshot }    from '@angular/router';
-import { AuthService }            from './auth.service';
+import { Injectable }          from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService }         from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(
-    // Not using but worth knowing about
-    next:  ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ) {
+  canActivate() {
     if (this.authService.isLoggedIn) { return true; }
     this.router.navigate(['/login']);
     return false;
