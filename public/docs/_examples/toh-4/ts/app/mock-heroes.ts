40d762aa6a5b299ab94cc689422fdeed56e8f71e
Thomas Shafer <thomasjshafer@gmail.com>
2016-07-08 10:29:21 -0700

    change var keyword to const for HEROES constant
    HEROES is a `const` and is defined as a `const` originally: https://github.com/angular/angular.io/blob/master/public/docs/_examples/toh-2/ts/app/app.component.ts#L10

--- public/docs/_examples/toh-4/ts/app/mock-heroes.ts
+++ public/docs/_examples/toh-4/ts/app/mock-heroes.ts
@@ -1,7 +1,7 @@
 // #docregion
 import { Hero } from './hero';
 
-export var HEROES: Hero[] = [
+export const HEROES: Hero[] = [
   {id: 11, name: 'Mr. Nice'},
   {id: 12, name: 'Narco'},
   {id: 13, name: 'Bombasto'},
