cc4e98f72bf80ee55fc91007360aa7a7d36fe53c
Brandon Roberts <robertsbt@gmail.com>
2016-07-19 21:59:34 -0500

    docs(router): Minor dev guide fixes

--- public/docs/_examples/router/ts/app/app.component.ts
+++ public/docs/_examples/router/ts/app/app.component.ts
@@ -13,7 +13,7 @@ import { HeroService }    from './heroes/hero.service';
     <h1 class="title">Component Router</h1>
     <nav>
       <a routerLink="/crisis-center" routerLinkActive="active"
-         routerLinkActiveOptions="{ exact: true }">Crisis Center</a>
+         [routerLinkActiveOptions]="{ exact: true }">Crisis Center</a>
       <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
       <a routerLink="/crisis-center/admin" routerLinkActive="active">Crisis Admin</a>
       <a routerLink="/login" routerLinkActive="active">Login</a>
