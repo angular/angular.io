38fe94df81dcded7b317cfa67584388e79d93612
Naomi Black <naomitraveller@gmail.com>
2016-07-27 00:24:54 -0700

    docs(qs-reboot): edits for the quickstart

--- public/docs/_examples/quickstart/ts/app/app.module2.ts
+++ public/docs/_examples/quickstart/ts/app/app.module2.ts
@@ -1,8 +1,12 @@
 // #docregion
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent}   from './app.component'
 
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
