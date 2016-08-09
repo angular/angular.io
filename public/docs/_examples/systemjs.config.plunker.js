325c004368960ff7286102fc688249b87a0adbff
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-09 20:32:30 +0200

    fix plunkers

--- public/docs/_examples/systemjs.config.plunker.js
+++ public/docs/_examples/systemjs.config.plunker.js
@@ -8,15 +8,15 @@
   var ngVer = '@2.0.0-rc.5'; // lock in the angular package version; do not let it float to current!
   var routerVer = '@3.0.0-rc.1'; // lock router version
   var formsVer = '@0.3.0'; // lock forms version
-  var routerDeprecatedVer = '@2.0.0-rc.2'; // Locked a this version
+  var routerDeprecatedVer = '@2.0.0-rc.2'; // temporarily until we update all the guides
 
   //map tells the System loader where to look for things
-  var  map = {
+  var map = {
     'app':                        'app',
 
     '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
-    '@angular/forms':             'https://npmcdn.com/@angular/forms' + formsVer,
     '@angular/router':            'https://npmcdn.com/@angular/router' + routerVer,
+    '@angular/forms':             'https://npmcdn.com/@angular/forms' + formsVer,
     '@angular/router-deprecated': 'https://npmcdn.com/@angular/router-deprecated' + routerDeprecatedVer,
     'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
     'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
@@ -35,12 +35,9 @@
     'common',
     'compiler',
     'core',
-    'forms',
     'http',
     'platform-browser',
     'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
     'upgrade',
   ];
 
@@ -51,7 +48,7 @@
   });
 
   // Add package entries for angular packages
-  ngPackageNames.forEach(function(pkgName) {
+  ngPackageNames.concat(['forms', 'router', 'router-deprecated']).forEach(function(pkgName) {
 
     // Bundled (~40 requests):
     packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
