dfd46af6049045fd217a520033406fd1be947fd7
Patrice Chalin <chalin@users.noreply.github.com>
2016-08-12 11:21:16 -0700

    docs(toh-6): post-RC5 Dart resync and TS fixes (#2095)
    * toh-6: trim spaces from cache file to simplify diff
    
    * toh-6: copy latest over cache before editing latest
    
    * docs(toh-6): post-RC5 Dart resync and TS fixes
    
    Contributes to #2077.
    
    TS-side changes include:
    - Merged three versions of `app/app.module{,1,2}.ts` into a single file
    and used docregions instead.
    - Misnamed files:
      - `rxjs-operators.ts` -> `rxjs-extensions.ts`
      - `hero-search.service.html` -> `hero-search.component.html`
    - Fixed BAD FILENAME error.
    
    Lint reports no errors and toh-6 e2e tests pass.

--- public/docs/_examples/toh-6/ts/app/app.module.ts
+++ public/docs/_examples/toh-6/ts/app/app.module.ts
@@ -1,24 +1,31 @@
-// #docregion
+// #docplaster
+// #docregion , v1, v2
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 
+// #enddocregion v1
 // Imports for loading & configuring the in-memory web api
-import { HttpModule, XHRBackend } from '@angular/http';
+import { XHRBackend } from '@angular/http';
 
 import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
 import { InMemoryDataService }               from './in-memory-data.service';
 
+// #docregion v1
 import { AppComponent }   from './app.component';
 import { routing }        from './app.routing';
 
 import { HeroesComponent }      from './heroes.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+// #enddocregion v1, v2
+// #docregion search
 import { HeroSearchComponent }  from './hero-search.component';
+// #docregion v1, v2
 
-import { HeroService }  from './hero.service';
-
+// #enddocregion search
 @NgModule({
   imports: [
     BrowserModule,
@@ -26,20 +33,25 @@ import { HeroService }  from './hero.service';
     routing,
     HttpModule
   ],
+  // #docregion search
   declarations: [
     AppComponent,
     HeroesComponent,
     DashboardComponent,
     HeroDetailComponent,
+    // #enddocregion v1, v2
     HeroSearchComponent
+    // #docregion v1, v2
   ],
+  // #enddocregion search
   providers: [
     HeroService,
+    // #enddocregion v1
     { provide: XHRBackend, useClass: InMemoryBackendService }, // in-mem server
     { provide: SEED_DATA,  useClass: InMemoryDataService }     // in-mem server data
+    // #docregion v1
   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
 }
-// #enddocregion
