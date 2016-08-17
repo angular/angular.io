a651255d511b369964c141a085e048405bcb6524
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-17 19:11:27 +0200

    chore: update karma for latest zone.js

--- public/docs/_examples/karma-test-shim.js
+++ public/docs/_examples/karma-test-shim.js
@@ -29,26 +29,20 @@ System.config({
 
 System.import('systemjs.config.js')
   .then(function () {
-    return Promise.all([
-      System.import('@angular/core/testing'),
-      System.import('@angular/platform-browser-dynamic/testing')
-    ])
-  })
-  .then(function (providers) {
-    var testing = providers[0];
-    var testingBrowser = providers[1];
-
-    testing.setBaseTestProviders(
-      testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
-      testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
-
-  })
-  .then(function() {
-    // Finally, load all spec files.
-    // This will run the tests directly.
-    return Promise.all(
-      allSpecFiles.map(function (moduleName) {
-        return System.import(moduleName);
-      }));
-  })
-  .then(__karma__.start, __karma__.error);
+    System.import('@angular/core/testing')
+      .then(function (coreTesting) {
+        return System.import('@angular/platform-browser-dynamic/testing').then(function (browserTesting) {
+          coreTesting.TestBed.initTestEnvironment(
+            browserTesting.BrowserDynamicTestingModule, browserTesting.platformBrowserDynamicTesting());
+        });
+      })
+      .then(function () {
+        // Finally, load all spec files.
+        // This will run the tests directly.
+        return Promise.all(
+          allSpecFiles.map(function (moduleName) {
+            return System.import(moduleName);
+          }));
+      })
+      .then(__karma__.start, __karma__.error);
+  });
