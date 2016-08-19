f6ff658db98893062422136940f19fdb548b97b2
Ward Bell <wardbell@hotmail.com>
2016-08-18 17:27:32 -0700

    docs(testing): remove popup example (study later)

--- public/docs/_examples/testing/ts/app/app.component.ts
+++ public/docs/_examples/testing/ts/app/app.component.ts
@@ -1,18 +1,8 @@
 // #docregion
 import { Component }   from '@angular/core';
-import { Router }      from '@angular/router';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
-export class AppComponent {
-  private toggle = false;
-
-  constructor(private router: Router) {  }
-
-  pop() {
-    this.toggle = !this.toggle;
-    this.router.navigate(['/', {outlets: { 'popup': this.toggle ? 'about' : null }} ]);
-  }
-}
+export class AppComponent { }
--- public/docs/_examples/testing/ts/app/app.module.ts
+++ public/docs/_examples/testing/ts/app/app.module.ts
@@ -19,7 +19,6 @@ import { SharedModule }     from './shared/shared.module';
     RouterModule.forRoot([
       { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
       { path: 'about', component: AboutComponent },
-      { path: 'about', component: AboutComponent, outlet: 'popup' },
       { path: 'heroes', loadChildren: 'app/hero/hero.module'}
     ]),
     SharedModule
