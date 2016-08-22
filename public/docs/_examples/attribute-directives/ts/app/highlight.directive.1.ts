9f0ff2e2a9de1011f735cfa67f2de9e804ec5a32
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-22 11:35:22 +0200

    docs(attribute-directives): add Renderer to style elements closes #2164

--- public/docs/_examples/attribute-directives/ts/app/highlight.directive.1.ts
+++ public/docs/_examples/attribute-directives/ts/app/highlight.directive.1.ts
@@ -1,10 +1,10 @@
 /* tslint:disable:no-unused-variable */
 // #docregion
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer } from '@angular/core';
 
 @Directive({ selector: '[myHighlight]' })
 export class HighlightDirective {
-    constructor(el: ElementRef) {
-       el.nativeElement.style.backgroundColor = 'yellow';
+    constructor(el: ElementRef, renderer: Renderer) {
+       renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'yellow');
     }
 }
