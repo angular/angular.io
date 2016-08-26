8e15829189e39ae199e62944aec173da4654714a
Ward Bell <wardbell@hotmail.com>
2016-08-25 19:12:15 -0700

    docs(ngmodule): fixes for defects discovered by John Papa (#2197)
    includes splitting FAQs into its own cookbook.

--- public/docs/_examples/ngmodule/ts/app/hero/hero.module.ts
+++ public/docs/_examples/ngmodule/ts/app/hero/hero.module.ts
@@ -7,14 +7,12 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroListComponent }   from './hero-list.component';
 import { routing }             from './hero.routing';
 
-/*
- * TODO: Remove THE HeroService class and provider after
- * https://github.com/angular/angular/pull/10579 lands
- */
+// TODO: Remove THE HeroService class in RC 6
 import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [ SharedModule, routing ],
+  // TODO: Remove in RC 6
   providers: [ HeroService ],
   declarations: [
     HeroComponent, HeroDetailComponent, HeroListComponent,
