dffd8efcbb4560048a25d7cd01ee89e00e823f63
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-28 23:37:44 +0200

    Jesus' edits

--- public/docs/_examples/cb-key-bindings/ts/app/main.ts
+++ public/docs/_examples/cb-key-bindings/ts/app/main.ts
@@ -1,18 +1,6 @@
 // #docregion
-// Import the native Angular services.
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// Import the application services and components.
-import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
-bootstrap( AppComponent )
-  .then(
-    () => {
-      console.info( 'Angular finished bootstrapping your application!' );
-    },
-    ( error ) => {
-      console.warn( 'Angular was not able to bootstrap your application.' );
-      console.error( error );
-    }
-  )
-;
+platformBrowserDynamic().bootstrapModule(AppModule);
