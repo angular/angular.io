bb122900be9da6fec61c3ec67c8f8d48d25e899e
Jesús Rodríguez <Foxandxss@gmail.com>
2016-09-01 17:24:15 +0200

    chore: fix quickstart

--- public/docs/_examples/quickstart/ts/app/app.module.ts
+++ public/docs/_examples/quickstart/ts/app/app.module.ts
@@ -2,7 +2,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent }   from './app.component';
+
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
