4c4934240b158d10991ec5e43c906ce5fbb4489e
Patrice Chalin <pchalin@gmail.com>
2016-09-07 17:30:30 -0700

    examples(lifecycle-hooks): update Dart sample to match TS
    Make equivalent TS code changes to Dart sample.
    
    This sync’s Dart with all #2177 changes pertinent to Dart.
    
    E2E tests pass now for Dart, Suites passed:
    -  public/docs/_examples/lifecycle-hooks/ts

--- public/docs/_examples/lifecycle-hooks/dart/lib/do_check_component.dart
+++ public/docs/_examples/lifecycle-hooks/dart/lib/do_check_component.dart
@@ -1,6 +1,4 @@
 // #docregion
-import 'dart:convert';
-
 import 'package:angular2/core.dart';
 
 class Hero {
@@ -23,7 +21,7 @@ class Hero {
       '.hero {background: LightYellow; padding: 8px; margin-top: 8px}',
       'p {background: Yellow; padding: 8px; margin-top: 8px}'
     ])
-class DoCheckComponent implements DoCheck, OnChanges {
+class DoCheckComponent implements DoCheck {
   @Input()
   Hero hero;
   @Input()
@@ -71,16 +69,6 @@ class DoCheckComponent implements DoCheck, OnChanges {
     changeDetected = false;
   }
   // #enddocregion ng-do-check
-
-  // Copied from OnChangesComponent
-  ngOnChanges(Map<String, SimpleChange> changes) {
-    changes.forEach((String propName, SimpleChange change) {
-      String cur = JSON.encode(change.currentValue);
-      String prev =
-          change.isFirstChange() ? "{}" : JSON.encode(change.previousValue);
-      changeLog.add('$propName: currentValue = $cur, previousValue = $prev');
-    });
-  }
 
   void reset() {
     changeDetected = true;
