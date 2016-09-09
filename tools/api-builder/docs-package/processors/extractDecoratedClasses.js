c5d31f8f15da96df6ee8bca90e9a12cbf0730604
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-09 10:27:11 +0100

    api-builder: render pipes with specialised template

--- tools/api-builder/docs-package/processors/extractDecoratedClasses.js
+++ tools/api-builder/docs-package/processors/extractDecoratedClasses.js
@@ -1,14 +1,14 @@
 var _ = require('lodash');
 
-module.exports = function extractDirectiveClassesProcessor(EXPORT_DOC_TYPES) {
+module.exports = function extractDecoratedClassesProcessor(EXPORT_DOC_TYPES) {
 
   // Add the "directive" docType into those that can be exported from a module
-  EXPORT_DOC_TYPES.push('directive');
+  EXPORT_DOC_TYPES.push('directive', 'pipe');
 
   return {
     $runAfter: ['processing-docs'],
     $runBefore: ['docs-processed'],
-    decoratorTypes: ['Directive', 'Component'],
+    decoratorTypes: ['Directive', 'Component', 'Pipe'],
     $process: function(docs) {
       var decoratorTypes = this.decoratorTypes;
 
@@ -17,9 +17,8 @@ module.exports = function extractDirectiveClassesProcessor(EXPORT_DOC_TYPES) {
         _.forEach(doc.decorators, function(decorator) {
 
           if (decoratorTypes.indexOf(decorator.name) !== -1) {
-            doc.docType = 'directive';
-
-            doc[decorator.name.toLowerCase() + 'Options'] = decorator.argumentInfo[0];
+            doc.docType = decorator.name.toLowerCase();
+            doc[doc.docType + 'Options'] = decorator.argumentInfo[0];
           }
         });
       });
