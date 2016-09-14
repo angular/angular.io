b5f3c933415d1e38eb3d5771b270651553f5b953
Filipe Silva <filipematossilva@gmail.com>
2016-09-14 02:42:49 +0100

    chore: update examples to TS2.0

--- public/docs/_examples/webpack/ts/config/webpack.test.js
+++ public/docs/_examples/webpack/ts/config/webpack.test.js
@@ -12,7 +12,7 @@ module.exports = {
     loaders: [
       {
         test: /\.ts$/,
-        loaders: ['ts', 'angular2-template-loader']
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
       },
       {
         test: /\.html$/,
