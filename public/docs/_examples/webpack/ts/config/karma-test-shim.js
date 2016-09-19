e9680b745f881f43b3e880704537a652ad44ad76
Jesús Rodríguez <Foxandxss@gmail.com>
2016-09-19 12:11:28 +0200

    docs(webpack): move to core-js in testing

--- public/docs/_examples/webpack/ts/config/karma-test-shim.js
+++ public/docs/_examples/webpack/ts/config/karma-test-shim.js
@@ -2,7 +2,7 @@
 Error.stackTraceLimit = Infinity;
 
 require('core-js/es6');
-require('reflect-metadata');
+require('core-js/es7/reflect');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
