dc9bd4d0a4bba7f52c0aa178cd69bcef72dce772
Filipe Silva <filipematossilva@gmail.com>
2016-09-24 01:27:58 +0100

    add base jit app

---
+++ public/docs/_examples/i18n/ts/app/app.component.ts
@@ -0,0 +1,27 @@
+// #docregion
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  // #docregion i18n-plural-pipe
+  messages: string[] = [];
+  messageMapping: {[k: string]: string} = {
+    '=0': 'No messages.',
+    '=1': 'One message.',
+    'other': '# messages.'
+  };
+  // #enddocregion i18n-plural-pipe
+  // #docregion i18n-select-pipe
+  gender = 'male';
+  genderMap = {
+    'male': 'Invite him.',
+    'female': 'Invite her.',
+    'other': 'Invite them.'
+  };
+  // #enddocregion i18n-select-pipe
+}
+
