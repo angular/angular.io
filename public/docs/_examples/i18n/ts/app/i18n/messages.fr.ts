76848c4c3329fea10aa7a551bde8fc5817724b5b
Filipe Silva <filipematossilva@gmail.com>
2016-09-24 02:30:19 +0100

    add translated jit example

---
+++ public/docs/_examples/i18n/ts/app/i18n/messages.fr.ts
@@ -0,0 +1,16 @@
+// #docregion
+export const TRANSLATION = `
+<?xml version="1.0" encoding="UTF-8" ?>
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+ <file source-language="en" datatype="plaintext" original="ng2.template" target-language="fr-fr">
+   <body>
+     <trans-unit id="53901890e9ecc1b020df02a275d815b32edcb99e" datatype="html">
+       <source>Hello i18n</source>
+       <target>Bonjour i18n</target>
+       <note priority="1" from="description">An introduction header for this sample</note>
+       <note priority="1" from="meaning">User welcome</note>
+     </trans-unit>
+   </body>
+ </file>
+</xliff>
+`;
--- public/docs/_examples/i18n/ts/app/main.ts
+++ public/docs/_examples/i18n/ts/app/main.ts
@@ -1,7 +1,19 @@
 // #docregion
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 
 import { AppModule } from './app.module';
+import { TRANSLATION } from './i18n/messages.fr';
 
+// Compile using french translations
 const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+platform.bootstrapModule(
+  AppModule,
+  {
+    providers: [
+      {provide: TRANSLATIONS, useValue: TRANSLATION},
+      {provide: TRANSLATIONS_FORMAT, useValue: 'xlf'},
+      {provide: LOCALE_ID, useValue: 'fr'}
+    ]
+  }
+);
