8b8ccf2206d568dd4ca4221bf87060cae5086de5
Filipe Silva <filipematossilva@gmail.com>
2016-09-26 12:04:58 +0100

    remove pipe usage

--- public/docs/_examples/i18n/ts/app/app.component.ts
+++ public/docs/_examples/i18n/ts/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  // #docregion i18n-plural-pipe
-  messages: string[] = [];
-  messageMapping: {[k: string]: string} = {
-    '=0': 'No messages.',
-    'one': 'One message.',
-    'other': '# messages.'
-  };
-  // #enddocregion i18n-plural-pipe
-  // #docregion i18n-select-pipe
-  gender = 'male';
-  genderMap = {
-    'male': 'Invite him.',
-    'female': 'Invite her.',
-    'other': 'Invite them.'
-  };
-  // #enddocregion i18n-select-pipe
-}
+export class AppComponent { }
 
