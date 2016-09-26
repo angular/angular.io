556e40695a1cf743891ab51b29bc5f23cf7d80a5
Brandon <robertsbt@gmail.com>
2016-09-25 20:56:12 -0500

    docs(toh): Replaced window.history with location service (#2439)

--- public/docs/_examples/toh-5/ts/app/hero-detail.component.ts
+++ public/docs/_examples/toh-5/ts/app/hero-detail.component.ts
@@ -1,10 +1,11 @@
 // #docplaster
 // #docregion , v2
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Hero }         from './hero';
+import { HeroService }  from './hero.service';
 
 @Component({
   moduleId: module.id,
@@ -23,8 +24,9 @@ export class HeroDetailComponent implements OnInit {
   // #docregion ctor
   constructor(
     private heroService: HeroService,
-    private route: ActivatedRoute) {
-  }
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
   // #enddocregion ctor
 
   // #docregion ngOnInit
@@ -39,7 +41,7 @@ export class HeroDetailComponent implements OnInit {
 
   // #docregion goBack
   goBack(): void {
-    window.history.back();
+    this.location.back();
   }
 // #enddocregion goBack
 }
--- public/docs/_examples/toh-6/ts/app/hero-detail.component.ts
+++ public/docs/_examples/toh-6/ts/app/hero-detail.component.ts
@@ -1,6 +1,7 @@
 // #docregion
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
 import { Hero }        from './hero';
 import { HeroService } from './hero.service';
@@ -16,8 +17,9 @@ export class HeroDetailComponent implements OnInit {
 
   constructor(
     private heroService: HeroService,
-    private route: ActivatedRoute) {
-  }
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.route.params.forEach((params: Params) => {
@@ -30,11 +32,11 @@ export class HeroDetailComponent implements OnInit {
   // #docregion save
   save(): void {
     this.heroService.update(this.hero)
-      .then(this.goBack);
+      .then(() => this.goBack());
   }
   // #enddocregion save
 
   goBack(): void {
-    window.history.back();
+    this.location.back();
   }
 }
