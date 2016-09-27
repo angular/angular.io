c04d1caef2f73911b573fc0e52534ef2ce5687cd
Patrice Chalin <pchalin@gmail.com>
2016-09-27 13:11:41 -0700

    minor edits to sample app titles to e2e tests pass
    Renamed “Angular 2” to “Angular” so that shared e2e tests pass.
    Tweak to QS prose.

--- public/docs/_examples/quickstart/dart/lib/app_component.dart
+++ public/docs/_examples/quickstart/dart/lib/app_component.dart
@@ -5,7 +5,7 @@ import 'package:angular2/core.dart';
 // #docregion metadata
 @Component(
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>')
+    template: '<h1>My First Angular App</h1>')
 // #enddocregion metadata
 // #docregion class
 class AppComponent {}
