f189496b1cbb68df38056aca17d87f69a0b323c5
Tim Xiao <xiaozhenzhong0708@hotmail.com>
2016-10-02 21:57:16 +0100

    docs: Added moduleId for styleUrls referencing in these two classes

--- public/docs/_examples/toh-5/ts/app/app.component.ts
+++ public/docs/_examples/toh-5/ts/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
   // #docregion template
   template: `
--- public/docs/_examples/toh-6/ts/app/app.component.ts
+++ public/docs/_examples/toh-6/ts/app/app.component.ts
@@ -3,6 +3,7 @@
 import { Component }          from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
 
   template: `
