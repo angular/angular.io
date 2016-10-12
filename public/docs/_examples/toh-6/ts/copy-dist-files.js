cdfe957ab8b194093b851f49d8bfdfdf49d5611e
Torgeir Helgevold <torgeirh@hotmail.com>
2016-10-12 04:44:49 -0400

    docs(toh-6-aot): add aot to toh-6 (#2496)
    * docs(toh-6-aot): add aot to toh-6
    
    s
    
    s
    
    s
    
    s
    
    s
    
    * docs(toh-6-aot): add aot to toh-6
    
    * docs(toh-6-aot): make aot e2e tests run in the same project
    Updates tooling for this purpose and also the prose.

---
+++ public/docs/_examples/toh-6/ts/copy-dist-files.js
@@ -0,0 +1,11 @@
+// #docregion
+var fs = require('fs');
+var resources = [
+  'node_modules/core-js/client/shim.min.js',
+  'node_modules/zone.js/dist/zone.min.js'
+];
+resources.map(function(f) {
+  var path = f.split('/');
+  var t = 'aot/' + path[path.length-1];
+  fs.createReadStream(f).pipe(fs.createWriteStream(t));
+});
