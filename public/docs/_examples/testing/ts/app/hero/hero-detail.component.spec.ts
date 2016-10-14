e9bf2bc5e879c11173d22f574f03cbf571d8d64a
Andras Sevcsik <sevcsik@gmail.com>
2016-10-14 14:31:29 +0200

    docs(testing): Added missing tick()
    Since rc.5, ngModel updates are async (see f444c11), so the change won't be reflected in the model immediately. Waiting a tick solves the issue.

--- public/docs/_examples/testing/ts/app/hero/hero-detail.component.spec.ts
+++ public/docs/_examples/testing/ts/app/hero/hero-detail.component.spec.ts
@@ -186,6 +186,8 @@ function heroModuleSetup() {
       // dispatch a DOM event so that Angular learns of input value change.
       page.nameInput.dispatchEvent(newEvent('input'));
 
+      // NgModel updates asynchronously
+      tick();
       // Tell Angular to update the output span through the title pipe
       fixture.detectChanges();
 
