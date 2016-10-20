c28b003a3c9ff3efff17015dd214d4598ad7c6d8
Ward Bell <wardbell@hotmail.com>
2016-10-19 23:17:50 -0700

    docs(HTTP Client): edit copy to conform to G-doc guidelines. (#2630)

--- public/docs/_examples/server-communication/ts/app/rxjs-operators.ts
+++ public/docs/_examples/server-communication/ts/app/rxjs-operators.ts
@@ -2,7 +2,7 @@
 // import 'rxjs/Rx'; // adds ALL RxJS statics & operators to Observable
 
 // See node_module/rxjs/Rxjs.js
-// Import just the rxjs statics and operators we need for THIS app.
+// Import just the rxjs statics and operators needed for THIS app.
 
 // Statics
 import 'rxjs/add/observable/throw';
--- public/docs/_examples/server-communication/ts/app/wiki/wiki.component.ts
+++ public/docs/_examples/server-communication/ts/app/wiki/wiki.component.ts
@@ -5,25 +5,19 @@ import { Observable }       from 'rxjs/Observable';
 import { WikipediaService } from './wikipedia.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-wiki',
-  template: `
-    <h1>Wikipedia Demo</h1>
-    <p><i>Fetches after each keystroke</i></p>
-
-    <input #term (keyup)="search(term.value)"/>
-
-    <ul>
-      <li *ngFor="let item of items | async">{{item}}</li>
-    </ul>
-  `,
-  providers: [WikipediaService]
+  templateUrl: 'wiki.component.html',
+  providers: [ WikipediaService ]
 })
 export class WikiComponent {
+  title   = 'Wikipedia Demo';
+  fetches = 'Fetches after each keystroke';
   items: Observable<string[]>;
-
-  constructor (private wikipediaService: WikipediaService) {}
 
   search (term: string) {
     this.items = this.wikipediaService.search(term);
   }
+
+  constructor (private wikipediaService: WikipediaService) { }
 }
