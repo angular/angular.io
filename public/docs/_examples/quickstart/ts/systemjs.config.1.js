2408bb149ae88f7d0b2330efa39124fed53efcfd
Steven <steven.pearce@nowyoyo.com>
2016-10-26 16:31:29 +0100

    fix for #2667 (#2668)
    ported changes made here https://github.com/angular/quickstart/commit/541bdc5f634e1142860c56cace247234edfaf74b

--- public/docs/_examples/quickstart/ts/systemjs.config.1.js
+++ public/docs/_examples/quickstart/ts/systemjs.config.1.js
@@ -23,10 +23,11 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -35,10 +36,6 @@
         defaultExtension: 'js'
       },
       rxjs: {
-        defaultExtension: 'js'
-      },
-      'angular-in-memory-web-api': {
-        main: './index.js',
         defaultExtension: 'js'
       }
     }
