ac1fec48a1839870ea50fc466df779631b2d8551
Patrice Chalin <pchalin@gmail.com>
2016-10-30 14:36:36 -0700

    copy-container and code-example - updates to work in new app

--- src/app/copy-container/copy-container.component.ts
+++ src/app/copy-container/copy-container.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-const Clipboard = require('clipboard');
+import { Component, AfterViewInit } from '@angular/core';
+
+// clipboard.min.js from the vendor folder.
+declare var Clipboard;
 
 @Component({
   selector: 'copy-container',
   templateUrl: 'copy-container.component.html'
 })
-export class CopyContainer implements OnInit, AfterViewInit {
+export class CopyContainerComponent implements AfterViewInit {
   isCopied: boolean = false;
   
-  constructor () {
-  }
-
-  ngOnInit() {
-  }
+  constructor () { }
   
   ngAfterViewInit() {
     const clipboard = new Clipboard(document.getElementsByClassName('js-copy-button'), {
