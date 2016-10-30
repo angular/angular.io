4d9e5d36d87e38a40a919031cadbfdeade25e14b
Patrice Chalin <pchalin@gmail.com>
2016-10-30 13:26:14 -0700

    ng g component ngio-cheatsheet + updates to app.component

---
+++ src/app/ngio-cheatsheet/ngio-cheatsheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngio-ngio-cheatsheet',
+  templateUrl: './ngio-cheatsheet.component.html',
+  styleUrls: ['./ngio-cheatsheet.component.css']
+})
+export class NgioCheatsheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
