cff384d4a1649052ebf8849c096b15118ad1bff7
Patrice Chalin <pchalin@gmail.com>
2016-10-30 14:26:18 -0700

    ng g component copy-container

---
+++ src/app/copy-container/copy-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngio-copy-container',
+  templateUrl: './copy-container.component.html',
+  styleUrls: ['./copy-container.component.css']
+})
+export class CopyContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
