64c4928b252bef10c8f9179905b2a04600fefe4f
Patrice Chalin <pchalin@gmail.com>
2016-10-30 14:26:18 -0700

    ng g component copy-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { LiveExampleComponent } from './live-example/live-example.component';
 import { DocInfoService } from './doc-info.service';
 import { NgioCheatsheetComponent } from './ngio-cheatsheet/ngio-cheatsheet.component';
 import { CodeExampleComponent } from './code-example/code-example.component';
+import { CopyContainerComponent } from './copy-container/copy-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LiveExampleComponent,
     NgioCheatsheetComponent,
-    CodeExampleComponent
+    CodeExampleComponent,
+    CopyContainerComponent
   ],
   imports: [
     BrowserModule,
