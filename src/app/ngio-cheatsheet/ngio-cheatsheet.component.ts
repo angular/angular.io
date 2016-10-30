fb8b5b8e0df0f6a7e8e1b2659def618b9cb2531d
Patrice Chalin <pchalin@gmail.com>
2016-10-30 13:32:44 -0700

    ngio-cheatsheet.component from ng2.io

--- src/app/ngio-cheatsheet/ngio-cheatsheet.component.ts
+++ src/app/ngio-cheatsheet/ngio-cheatsheet.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { DocInfoService, NgLang } from '../doc-info.service';
 
 @Component({
-  selector: 'ngio-ngio-cheatsheet',
-  templateUrl: './ngio-cheatsheet.component.html',
-  styleUrls: ['./ngio-cheatsheet.component.css']
+  selector: 'ngio-cheatsheet',
+  templateUrl: 'ngio-cheatsheet.component.html'
 })
-export class NgioCheatsheetComponent implements OnInit {
+export class NgioCheatSheetComponent implements OnInit {
+  private url: string;
+  isLoaded: boolean = false;
+  currentEnvironment: any;
+  version: any;
+  sections: any;
 
-  constructor() { }
+  // NOTE (ericjim): the src input from the old project is not being used. We are hardcoding the json
+  // file name in `URL` for now. 
 
-  ngOnInit() {
+  constructor(
+    private http: Http,
+    private docInfoSvc: DocInfoService
+  ) {
+    this.url = `assets/${docInfoSvc.ngLang}/${docInfoSvc.vers}/guide/cheatsheet.json`
   }
 
+  ngOnInit() {
+    this.tryUrlAndSetToSrc();
+  }
+
+  tryUrlAndSetToSrc() {
+    this.http.get(this.url)
+      .map(resp => resp.json())
+      .subscribe((data) => {
+        this.currentEnvironment = data.currentEnvironment;
+        this.version = data.version;
+        this.sections = data.sections;
+        this.isLoaded = true;
+      }, 
+      (e) => { 
+        console.error(e); 
+      });
+  }
 }
