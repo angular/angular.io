ae35f0e017104118cfda71c7bc5944cb99dc2e19
Ward Bell <wardbell@hotmail.com>
2016-11-02 21:50:38 -0700

    docs(lifecycle-hooks): `SimpleChange` -> `SimpleChanges` #2741 (#2742)

--- public/docs/_examples/lifecycle-hooks/ts/app/counter.component.ts
+++ public/docs/_examples/lifecycle-hooks/ts/app/counter.component.ts
@@ -1,7 +1,7 @@
 // #docregion
 import {
   Component, Input,
-  OnChanges, SimpleChange,
+  OnChanges, SimpleChanges,
 } from '@angular/core';
 
 import { LoggerService }  from './logger.service';
@@ -22,7 +22,7 @@ export class MyCounterComponent implements OnChanges {
   @Input() counter: number;
   changeLog: string[] = [];
 
-  ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
+  ngOnChanges(changes: SimpleChanges) {
 
     // Empty the changeLog whenever counter goes to zero
     // hint: this is a way to respond programmatically to external value changes.
@@ -36,7 +36,6 @@ export class MyCounterComponent implements OnChanges {
     let prev = JSON.stringify(chng.previousValue); // first time is {}; after is integer
     this.changeLog.push(`counter: currentValue = ${cur}, previousValue = ${prev}`);
   }
-
 }
 
 /***************************************/
