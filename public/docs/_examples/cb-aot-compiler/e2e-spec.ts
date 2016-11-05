a5104f0f19424541487d15f3b875efb005691258
Ward Bell <wardbell@hotmail.com>
2016-11-05 13:13:02 -0700

    docs(quickstart): reboot edition

--- public/docs/_examples/cb-aot-compiler/e2e-spec.ts
+++ public/docs/_examples/cb-aot-compiler/e2e-spec.ts
@@ -1,4 +1,4 @@
-'use strict'; // necessary for es6 output in node 
+'use strict'; // necessary for es6 output in node
 
 import { browser, element, by } from 'protractor';
 
@@ -11,7 +11,7 @@ describe('AOT Compilation', function () {
 
     it('should load page and click button', function (done) {
       let headingSelector = element.all(by.css('h1')).get(0);
-      expect(headingSelector.getText()).toEqual('My First Angular App');
+      expect(headingSelector.getText()).toEqual('Hello Angular');
 
       expect(element.all(by.xpath('//div[text()="Magneta"]')).get(0).isPresent()).toBe(true);
       expect(element.all(by.xpath('//div[text()="Bombasto"]')).get(0).isPresent()).toBe(true);
--- public/docs/_examples/setup/ts/app/main.ts
+++ public/docs/_examples/setup/ts/app/main.ts
@@ -1,5 +1,7 @@
+// #docregion
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
+import { AppModule }              from './app.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
+// #enddocregion
+
+/* This file is excluded in plunkers */
