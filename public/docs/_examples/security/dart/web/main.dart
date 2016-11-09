9f56e96bf03181498ccbab63b77f21e369cd1882
Patrice Chalin <pchalin@gmail.com>
2016-11-09 07:40:14 -0800

    docs(security/dart): new chapter + sample
    Current version has 2/3 of the TS counterpart.

---
+++ public/docs/_examples/security/dart/web/main.dart
@@ -0,0 +1,10 @@
+// #docregion
+import 'package:angular2/platform/browser.dart';
+
+// #docregion import
+import 'package:angular_security/app_component.dart';
+// #enddocregion import
+
+void main() {
+  bootstrap(AppComponent);
+}
