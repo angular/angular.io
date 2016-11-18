cc3b44044409afe796e70b5d99c9375c95cc20a7
Patrice Chalin <pchalin@gmail.com>
2016-11-18 07:50:51 -0800

    example(pipes): Dart e2e fix
    Match TS example and only bootstrap AppComponent.

--- public/docs/_examples/pipes/dart/web/main.dart
+++ public/docs/_examples/pipes/dart/web/main.dart
@@ -1,9 +1,7 @@
 import 'package:angular2/platform/browser.dart';
 
 import 'package:pipe_examples/app_component.dart';
-import 'package:pipe_examples/hero_birthday1_component.dart';
 
-main() {
+void main() {
   bootstrap(AppComponent);
-  bootstrap(HeroBirthdayComponent);
 }
