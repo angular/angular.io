66828edac26aca1d880048a3798a8481a7a7986d
Ward Bell <wardbell@hotmail.com>
2016-11-19 10:44:20 -0800

    docs(quickstart): update per @chalin comments in PR #2762

--- public/docs/_examples/setup/ts/app/main.ts
+++ public/docs/_examples/setup/ts/app/main.ts
@@ -1,7 +1,5 @@
 // #docregion
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
-platformBrowserDynamic().bootstrapModule(AppModule);
-// #enddocregion
 
-/* This file is excluded in plunkers */
+platformBrowserDynamic().bootstrapModule(AppModule);
