bcab06403c493a391a4eafd00311c8c0957fe840
Wenchen Li <neolwc@users.noreply.github.com>
2016-11-21 15:59:00 -0500

    Fix #2066 (#2800)

--- public/docs/_examples/forms/ts/app/hero-form.component.ts
+++ public/docs/_examples/forms/ts/app/hero-form.component.ts
@@ -29,20 +29,10 @@ export class HeroFormComponent {
   // #enddocregion first
 
   // #docregion final
-  // Reset the form with a new hero AND restore 'pristine' class state
-  // by toggling 'active' flag which causes the form
-  // to be removed/re-added in a tick via NgIf
-  // TODO: Workaround until NgForm has a reset method (#6822)
   // #docregion new-hero
-  active = true;
-
   // #docregion new-hero-v1
   newHero() {
     this.model = new Hero(42, '', '');
-  // #enddocregion new-hero-v1
-    this.active = false;
-    setTimeout(() => this.active = true, 0);
-  // #docregion new-hero-v1
   }
   // #enddocregion new-hero-v1
   // #enddocregion new-hero
