ec2e6cb2c7003d58002a63d14633bf0e32e182f2
Ward Bell <wardbell@hotmail.com>
2016-11-21 17:13:21 -0800

    [WIP] docs(quickstart): QuickStart reboot (#2762)

--- public/docs/_examples/setup/ts/app/app.module.ts
+++ public/docs/_examples/setup/ts/app/app.module.ts
@@ -1,8 +1,11 @@
 // #docregion
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
