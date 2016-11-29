efcb906981e41a2a7c59d5e273f177fec82c64a7
Ward Bell <wardbell@hotmail.com>
2016-11-29 15:39:53 -0800

    chore(cb-component-communication): tweak code/update TS version (#2900)
    Simplify name-child component.
    Update TS version in package.json and the web systemjs.configs
    Inspired by issue #2881

--- public/docs/_examples/cb-component-communication/ts/app/name-child.component.ts
+++ public/docs/_examples/cb-component-communication/ts/app/name-child.component.ts
@@ -3,18 +3,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'name-child',
-  template: `
-    <h3>"{{name}}"</h3>
-  `
+  template: '<h3>"{{name}}"</h3>'
 })
 export class NameChildComponent {
-  _name: string = '<no name set>';
+  private _name = '';
 
   @Input()
   set name(name: string) {
     this._name = (name && name.trim()) || '<no name set>';
   }
 
-  get name() { return this._name; }
+  get name(): string { return this._name; }
 }
 // #enddocregion
--- public/docs/_examples/cb-component-communication/ts/app/name-parent.component.ts
+++ public/docs/_examples/cb-component-communication/ts/app/name-parent.component.ts
@@ -4,10 +4,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'name-parent',
   template: `
-    <h2>Master controls {{names.length}} names</h2>
-    <name-child *ngFor="let name of names"
-      [name]="name">
-    </name-child>
+  <h2>Master controls {{names.length}} names</h2>
+  <name-child *ngFor="let name of names" [name]="name"></name-child>
   `
 })
 export class NameParentComponent {
