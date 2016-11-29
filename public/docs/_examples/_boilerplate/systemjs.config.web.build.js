93dde05aea98a8df7d0f43c2e89b8439553183af
Ward Bell <wardbell@hotmail.com>
2016-11-29 02:35:50 -0800

    chore(cb-component-communication): tweak code/update TS version Simplify name-child component. Update TS version in package.json and the web systemjs.configs Inspired by issue #2881

--- public/docs/_examples/_boilerplate/systemjs.config.web.build.js
+++ public/docs/_examples/_boilerplate/systemjs.config.web.build.js
@@ -62,8 +62,8 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'ts':                        'npm:plugin-typescript@4.0.10/lib/plugin.js',
-      'typescript':                'npm:typescript@2.0.3/lib/typescript.js',
+      'ts':                        'npm:plugin-typescript@5.2.7/lib/plugin.js',
+      'typescript':                'npm:typescript@2.0.10/lib/typescript.js',
 
     },
     // packages tells the System loader how to load when no filename and/or no extension
