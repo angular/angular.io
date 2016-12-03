9fd3fa037d24b46da6b67bb061ce8c856932aa94
Jesús Rodríguez <Foxandxss@gmail.com>
2016-12-03 02:25:31 +0100

    docs(i18n): add new features (#2740)

--- public/docs/_examples/cb-i18n/ts/app/app.component.ts
+++ public/docs/_examples/cb-i18n/ts/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  wolves = 0;
+  gender = 'f';
+  fly = true;
+  logo = 'https://angular.io/resources/images/logos/angular2/angular.png';
+  inc(i: number) {
+    this.wolves = Math.min(5, Math.max(0, this.wolves + i));
+  }
+  male()   { this.gender = 'm'; }
+  female() { this.gender = 'f'; }
+}
 
