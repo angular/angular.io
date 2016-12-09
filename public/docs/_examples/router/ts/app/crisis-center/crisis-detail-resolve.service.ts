cf973263448a7a237b18c9d688d04df46f0b23c4
Victor Berchet <victor@suumit.com>
2016-12-09 05:56:14 -0800

    fix(router): fix Resolve docs and Resolve.resolve signature (#2954)

--- public/docs/_examples/router/ts/app/crisis-center/crisis-detail-resolve.service.ts
+++ public/docs/_examples/router/ts/app/crisis-center/crisis-detail-resolve.service.ts
@@ -1,6 +1,6 @@
 // #docregion
 import { Injectable }             from '@angular/core';
-import { Router, Resolve,
+import { Router, Resolve, RouterStateSnapshot,
          ActivatedRouteSnapshot } from '@angular/router';
 
 import { Crisis, CrisisService } from './crisis.service';
@@ -9,7 +9,7 @@ import { Crisis, CrisisService } from './crisis.service';
 export class CrisisDetailResolve implements Resolve<Crisis> {
   constructor(private cs: CrisisService, private router: Router) {}
 
-  resolve(route: ActivatedRouteSnapshot): Promise<Crisis>|boolean {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<Crisis> {
     let id = route.params['id'];
 
     return this.cs.getCrisis(id).then(crisis => {
@@ -17,7 +17,7 @@ export class CrisisDetailResolve implements Resolve<Crisis> {
         return crisis;
       } else { // id not found
         this.router.navigate(['/crisis-center']);
-        return false;
+        return null;
       }
     });
   }
