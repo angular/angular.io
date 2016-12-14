9a3cbd611f2f8343146187dc3555e39d05178abf
Ward Bell <wardbell@hotmail.com>
2016-12-14 13:30:25 -0800

    docs(http): show how to override default `RequestOptions` like headers (#2978)

--- public/docs/_examples/server-communication/ts/app/app.module.1.ts
+++ public/docs/_examples/server-communication/ts/app/app.module.1.ts
@@ -16,7 +16,8 @@ import { AppComponent } from './app.component';
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 
 
