ed67a063129c5927372482a9515dfaf4c2a079da
Ward Bell <wardbell@hotmail.com>
2016-12-20 14:07:07 -0800

    cosmetics on app.module.4.ts

--- public/docs/_examples/router/ts/app/app.module.4.ts
+++ public/docs/_examples/router/ts/app/app.module.4.ts
@@ -5,22 +5,19 @@ import { NgModule }       from '@angular/core';
 import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }         from './app.component';
-import { PageNotFoundComponent }from './not-found.component';
-import { AppRoutingModule }     from './app-routing.module';
-
-import { HeroesModule }           from './heroes/heroes.module';
+import { AppComponent }            from './app.component';
+import { PageNotFoundComponent }   from './not-found.component';
+import { AppRoutingModule }        from './app-routing.module';
+import { HeroesModule }            from './heroes/heroes.module';
 // #docregion crisis-center-module
-import { CrisisCenterModule }     from './crisis-center/crisis-center.module';
+import { CrisisCenterModule }      from './crisis-center/crisis-center.module';
 // #enddocregion crisis-center-module
-
 import { ComposeMessageComponent } from './compose-message.component';
 // #docregion admin-module
-import { AdminModule }            from './admin/admin.module';
+import { AdminModule }             from './admin/admin.module';
 // #docregion crisis-center-module
 
-import { DialogService }          from './dialog.service';
-
+import { DialogService }           from './dialog.service';
 
 @NgModule({
   imports: [
