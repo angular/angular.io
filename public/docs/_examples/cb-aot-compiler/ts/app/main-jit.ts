633023d695b4ebf3e6f542eb02d3b36dfa64c766
Ward Bell <wardbell@hotmail.com>
2017-01-14 14:44:47 -0800

    docs(aot-cookbook): workflow suggestion; show how to run with JIT

--- public/docs/_examples/cb-aot-compiler/ts/app/main-jit.ts
+++ public/docs/_examples/cb-aot-compiler/ts/app/main-jit.ts
@@ -1,6 +1,6 @@
 // #docregion
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule }              from './app.module';
 
+console.log('Running JIT compiled');
 platformBrowserDynamic().bootstrapModule(AppModule);
--- public/docs/_examples/cb-aot-compiler/ts/app/main.ts
+++ public/docs/_examples/cb-aot-compiler/ts/app/main.ts
@@ -1,6 +1,6 @@
 // #docregion
 import { platformBrowser }    from '@angular/platform-browser';
-
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
 
+console.log('Running AOT compiled');
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
