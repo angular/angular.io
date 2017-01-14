f12105dc0dc425417714bd1b483efc4c31b954fb
Ward Bell <wardbell@hotmail.com>
2017-01-14 12:54:33 -0800

    chore(inheritance): gitignore gen'd css and skip rollup warn on 'this'

--- public/docs/_examples/inheritance/ts/rollup-config.js
+++ public/docs/_examples/inheritance/ts/rollup-config.js
@@ -10,6 +10,17 @@ export default {
   dest: 'dist/build.js', // output a single application bundle
   sourceMap: false,
   format: 'iife',
+  onwarn: function(warning) {
+    // Skip certain warnings
+
+    // should intercept ... but doesn't in some rollup versions
+    if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
+    // intercepts in some rollup versions
+    if ( warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
+
+    // console.warn everything else
+    console.warn( warning.message );
+  },
   plugins: [
       nodeResolve({jsnext: true, module: true}),
       // #docregion commonjs
