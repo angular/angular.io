3ec94d16eb594922ebbbbbc1a9a9f017950ff2fe
Ward Bell <wardbell@hotmail.com>
2017-01-14 12:44:08 -0800

    chore(aot-cookbook): gitignore gen'd css and skip rollup warn on 'this'

--- public/docs/_examples/cb-aot-compiler/ts/rollup-config.js
+++ public/docs/_examples/cb-aot-compiler/ts/rollup-config.js
@@ -10,6 +10,17 @@ export default {
   dest: 'dist/build.js', // output a single application bundle
   sourceMap: false,
   format: 'iife',
+  onwarn: function(warning) {
+    // skip certain warnings
+
+    // Should intercept ... but doesn't in some rollup versions
+    if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
+    // Intercepts in some rollup versions
+    if ( warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
+
+    // console.warn everything else
+    console.warn ? console.warn( warning.message ) : console.log( warning.message ) ;
+  },
   plugins: [
       nodeResolve({jsnext: true, module: true}),
       // #docregion commonjs
