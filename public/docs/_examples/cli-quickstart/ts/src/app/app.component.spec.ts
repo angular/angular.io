a7e4f307406beafda697093d1723ad7c8e55cbd4
Filipe Silva <filipematossilva@gmail.com>
2017-01-23 23:44:30 +0000

    incorporate Ward's feedback

--- public/docs/_examples/cli-quickstart/ts/src/app/app.component.spec.ts
+++ public/docs/_examples/cli-quickstart/ts/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
