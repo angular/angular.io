a1f9c0b483b732965d56e338d9d33ddc7a1809fb
Filipe Silva <filipematossilva@gmail.com>
2017-01-30 13:30:57 +0000

    move rest of ts files

--- public/docs/_examples/dependency-injection/ts/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }           from './app.module';
-
-// #docregion bootstrap
-platformBrowserDynamic().bootstrapModule(AppModule);
-// #enddocregion bootstrap
