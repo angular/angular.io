36fc6ed47ea4e18c0ba8f6ace4341b6221128308
Ward Bell <wardbell@hotmail.com>
2017-01-30 11:16:09 -0800

    chore(quickstart): restore app.module and main We seemed to create more confusion by omitting app.module (and main) in QS than we gained in simplicity of QS Every other example requires these files. This PR restores them to QS _but doesn't talk about them_ in QS, leaving _that_ to the Setup guide.

--- public/docs/_examples/_boilerplate/src/systemjs.config.web.js
+++ public/docs/_examples/_boilerplate/src/systemjs.config.web.js
@@ -66,58 +66,6 @@
     }
   });
 
-  if (global.autoBootstrap) { bootstrap(); }
-
-  // Bootstrap with a default `AppModule`
-  // ignore an `app/app.module.ts` and `app/main.ts`, even if present
-  // This function exists primarily (exclusively?) for the QuickStart
-  function bootstrap() {
-    console.log('Auto-bootstrapping');
-
-    // Stub out `app/main.ts` so System.import('app') doesn't fail if called in the index.html
-    System.set(System.normalizeSync('app/main.ts'), System.newModule({ }));
-
-    // bootstrap and launch the app (equivalent to standard main.ts)
-    Promise.all([
-      System.import('@angular/platform-browser-dynamic'),
-      getAppModule()
-    ])
-    .then(function (imports) {
-      var platform = imports[0];
-      var app      = imports[1];
-      platform.platformBrowserDynamic().bootstrapModule(app.AppModule);
-    })
-    .catch(function(err){ console.error(err); });
-  }
-
-  // Make the default AppModule
-  // returns a promise for the AppModule
-  function getAppModule() {
-    console.log('Making a bare-bones, default AppModule');
-
-    return Promise.all([
-      System.import('@angular/core'),
-      System.import('@angular/platform-browser'),
-      System.import('app/app.component')
-    ])
-    .then(function (imports) {
-
-      var core    = imports[0];
-      var browser = imports[1];
-      var appComp = imports[2].AppComponent;
-
-      var AppModule = function() {}
-
-      AppModule.annotations = [
-        new core.NgModule({
-          imports:      [ browser.BrowserModule ],
-          declarations: [ appComp ],
-          bootstrap:    [ appComp ]
-        })
-      ]
-      return {AppModule: AppModule};
-    })
-  }
 })(this);
 
 /*
