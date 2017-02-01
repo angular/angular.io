01d0a8a92421d954eb4f51e53cf508fff199e85b
Filipe Silva <filipematossilva@gmail.com>
2017-02-01 19:20:12 +0000

    update new reactive forms example

--- public/docs/_examples/reactive-forms/ts/src/main.ts
+++ public/docs/_examples/reactive-forms/ts/src/main.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-unused-variable
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }  from './app.module';  // just the final version
-import { DemoModule } from './demo.module'; // demo picker
+import { AppModule }  from './app/app.module';  // just the final version
+import { DemoModule } from './app/demo.module'; // demo picker
 
 platformBrowserDynamic().bootstrapModule(DemoModule); // (AppModule);
