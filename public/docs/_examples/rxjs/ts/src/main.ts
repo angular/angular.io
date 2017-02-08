fba087bab5e0283ba5aadc7e21775b4902d022d5
Brandon Roberts <robertsbt@gmail.com>
2017-02-07 19:58:11 -0600

    Refactored files to fit new folder structure

--- public/docs/_examples/rxjs/ts/src/main.ts
+++ public/docs/_examples/rxjs/ts/src/main.ts
@@ -1,6 +1,6 @@
 // #docregion
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
