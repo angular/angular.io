315b63176ce79c024f2a8b639c5fcc3bdd6f2e92
Jesus Rodriguez <Foxandxss@gmail.com>
2017-02-20 21:00:30 +0100

    docs(webpack): one step closer to CLI

--- public/docs/_examples/webpack/ts/config/karma.conf.js
+++ public/docs/_examples/webpack/ts/config/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: true
   };
 
--- public/docs/_examples/webpack/ts/config/webpack.common.js
+++ public/docs/_examples/webpack/ts/config/webpack.common.js
@@ -24,10 +24,12 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        loaders: [{
-          loader: 'awesome-typescript-loader',
-          options: { configFileName: helpers.root('src', 'tsconfig.json') }
-        } , 'angular2-template-loader']
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('src', 'tsconfig.json') }
+          } , 'angular2-template-loader'
+        ]
       },
       {
         test: /\.html$/,
