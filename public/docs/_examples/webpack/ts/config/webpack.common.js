037b50e57d4b11837d27dbaeb49722c1e9128b77
Dave Parsons <dualrudder@gmail.com>
2017-02-21 16:23:10 -0800

    Fix build for webpack example

--- public/docs/_examples/webpack/ts/config/webpack.common.js
+++ public/docs/_examples/webpack/ts/config/webpack.common.js
@@ -24,10 +24,7 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        loaders: [{
-          loader: 'awesome-typescript-loader',
-          options: { configFileName: helpers.root('src', 'tsconfig.json') }
-        } , 'angular2-template-loader']
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
       },
       {
         test: /\.html$/,
