2560787481f71d9517b8d00bebde96ed7143b1e2
Kapunahele Wong <kapunahelewong@gmail.com>
2017-03-08 11:51:56 -0500

    docs(http-client): edit copy and  true TOC to doc shape

--- public/docs/_examples/server-communication/ts/src/app/toh/hero.service.ts
+++ public/docs/_examples/server-communication/ts/src/app/toh/hero.service.ts
@@ -57,7 +57,7 @@ export class HeroService {
   // #docregion error-handling
 
   private handleError (error: Response | any) {
-    // In a real world app, we might use a remote logging infrastructure
+    // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
