2fbca288c24773c5f7fc5e46d5d4ff502f844c20
Patrice Chalin <chalin@users.noreply.github.com>
2017-03-14 10:48:21 -0700

    docs(server-comm): copyedits (#3373)
    - Minor prose copyedits.
    - Renamed `HeroService.addHero()` to `create()`, so as to match the ToH
    sample.

--- public/docs/_examples/server-communication/ts/src/app/toh/hero-list.component.ts
+++ public/docs/_examples/server-communication/ts/src/app/toh/hero-list.component.ts
@@ -31,9 +31,9 @@ export class HeroListComponent implements OnInit {
   // #enddocregion getHeroes
 
   // #docregion addHero
-  addHero (name: string) {
+  addHero(name: string) {
     if (!name) { return; }
-    this.heroService.addHero(name)
+    this.heroService.create(name)
                      .subscribe(
                        hero  => this.heroes.push(hero),
                        error =>  this.errorMessage = <any>error);
