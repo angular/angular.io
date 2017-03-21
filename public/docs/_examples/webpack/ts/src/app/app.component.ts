ea638482e583b3b1729b5114ecdd0cbe8c3251fb
Jesús Rodríguez <Foxandxss@gmail.com>
2017-03-21 05:37:49 +0100

    docs(webpack): remove ts-snippets (#3398)

--- public/docs/_examples/webpack/ts/src/app/app.component.ts
+++ public/docs/_examples/webpack/ts/src/app/app.component.ts
@@ -1,8 +1,12 @@
+// #docplaster
 // #docregion
+// #docregion component
 import { Component } from '@angular/core';
 
+// #enddocregion component
 import '../assets/css/styles.css';
 
+// #docregion component
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
