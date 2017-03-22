21adfa05d5f0e011b764ab4624b419ee88d312b1
Brad Christie <brad.christie@gmail.com>
2017-03-22 18:59:24 -0400

    create() should return Hero model.
    To keep things uniform, i suspect this should be cast like `getHero` & `getHeroes`.

--- public/docs/_examples/toh-6/ts/src/app/hero.service.ts
+++ public/docs/_examples/toh-6/ts/src/app/hero.service.ts
@@ -61,7 +61,7 @@ export class HeroService {
     return this.http
       .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
       .toPromise()
-      .then(res => res.json().data)
+      .then(res => res.json().data as Hero)
       .catch(this.handleError);
   }
   // #enddocregion create
