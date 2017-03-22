d38aedc186f04552ff34e9ca5b4d48faebd4fd1c
Filipe Silva <filipematossilva@gmail.com>
2017-03-22 18:22:03 +0000

    remove unused changes

--- public/docs/_examples/_boilerplate/src/systemjs.config.js
+++ public/docs/_examples/_boilerplate/src/systemjs.config.js
@@ -31,8 +31,7 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'hero-profile': 'npm:hero-profile/bundles/hero-profile.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
