16f9c855e343041d6b3732f422aa7b59438af11b
urigo <uri.goldshtein@gmail.com>
2017-03-22 02:53:39 +0100

    docs(graphql-cookbook): Fix inner property value

--- public/docs/_examples/heroes-graphql-starter/ts/src/app/app.component.ts
+++ public/docs/_examples/heroes-graphql-starter/ts/src/app/app.component.ts
@@ -15,5 +15,5 @@ import { Component }          from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Tour of Heroes - GraphQL Starter';
 }
