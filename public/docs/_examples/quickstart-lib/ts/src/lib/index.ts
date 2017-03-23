c5e7de33afb3cb8b9114977883951b7698a7f7a5
Filipe Silva <filipematossilva@gmail.com>
2017-03-23 14:57:46 +0000

    update folder struture

--- public/docs/_examples/quickstart-lib/ts/src/lib/index.ts
+++ public/docs/_examples/quickstart-lib/ts/src/lib/index.ts
@@ -1,4 +1,4 @@
 // #docregion
-export { LibComponent } from './component/lib.component';
-export { LibService } from './service/lib.service';
-export { LibModule } from './module';
+export { LibComponent } from './src/component/lib.component';
+export { LibService } from './src/service/lib.service';
+export { LibModule } from './src/module';
